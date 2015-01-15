8a350bca42968660ca043f0249a0ee8b4ac1b212
David Valdman <dmvaldman@gmail.com>
2015-01-15 14:50:03 +0700

    PaginatedLayout now inherits from View

--- widgets/PaginatedLayout.js
+++ widgets/PaginatedLayout.js
@@ -6,109 +6,162 @@ define(function(require, exports, module) {
     var RenderNode = require('famous/core/RenderNode');
     var Modifier = require('famous/core/Modifier');
     var EventHandler = require('../core/EventHandler');
+    var ViewSequence = require('../core/ViewSequence');
+    var View = require('../views/View');
 
-    function PaginatedLayout(){
+    function PaginatedLayout(options){
+        View.apply(this, arguments);
+
+        // state
+        this.index = 0;
+        this.pages = new ViewSequence([]);
+        this.progress = 0;
+        this.dotsWidth = 0;
+        this.size = null;
+
+        // subcomponents
+
         this.scrollview = new Scrollview({
             direction : 0,
             paginated : true,
             margin: Infinity
         });
-        window.scrollview = this.scrollview;
 
-        this.dots = [];
-        this.index = 0;
+        this.scrollview.sequenceFrom(this.pages);
 
-        this.currentDotPosition = new Transitionable(0);
-        this.currentDotOpacity = new Transitionable(1);
-        this.dotsWidth = 0;
+        var currentDotPosition = new Transitionable(0);
+        var currentDotOpacity = new Transitionable(1);
+
+        var currentDotModifier = new Modifier({
+            opacity : currentDotOpacity,
+            transform : function(){
+                return Transform.translate(currentDotPosition.get(), 0, 0);
+            }.bind(this)
+        });
+
+        var currentDot = new Surface({
+            size : [2*this.options.dotRadius, 2*this.options.dotRadius],
+            properties : {
+                borderRadius : '50%',
+                background : this.options.dotColor
+            }
+        });
 
         var dotsModifier = new Modifier({
-            size : function(){return [this.dotsWidth, 10]}.bind(this),
+            size : function(){return [this.dotsWidth, 2 * this.options.dotRadius]}.bind(this),
             transform : Transform.inFront,
             origin : [.5,.5],
             align : [.5,.9]
         });
 
-        var currentDotModifier = new Modifier({
-            opacity : this.currentDotOpacity,
-            transform : function(){
-                return Transform.translate(this.currentDotPosition.get(), 0, 0)
-            }.bind(this)
-        });
-
         this.dotsNode = new RenderNode(dotsModifier);
+        this.dotsNode.add(currentDotModifier).add(currentDot);
 
-        this.currentDot = new Surface({
-            size : [10,10],
-            properties : {
-                borderRadius : '50%',
-                background : 'white'
-            }
-        });
+        this.add(this.scrollview);
+        this.add(this.dotsNode);
 
-        this.dotsNode.add(currentDotModifier).add(this.currentDot);
-
-        this._eventInput = new EventHandler();
-        this._eventOutput = new EventHandler();
-        EventHandler.setInputHandler(this, this._eventInput);
-        EventHandler.setOutputHandler(this, this._eventOutput);
+        // eventing
 
         this._eventInput.pipe(this.scrollview);
         this.subscribe(this.scrollview);
 
-//        this._eventInput.on('pageChange', function(data){
-//            var index = data.index;
-//            var position = index * 14;
-//            this.currentDotOpacity.set(0);
-//            this.currentDotOpacity.set(1, {duration : 500});
-//            this.currentDotPosition.set(position)
-//        }.bind(this));
+        switch (this.options.transition){
+            case PaginatedLayout.TRANSITION.CONTINUOUS:
+                this._eventInput.on('update', function(data){
+                    currentDotPosition.set(data.progress);
+                    var index = data.index;
+                    var progress = data.progress;
 
-//        this._eventInput.on('settle', function(){
-//            var index = this.scrollview.getCurrentIndex();
-//            if (index == this.index) return;
-//            var position = index * 14;
-//            this.currentDotOpacity.set(0);
-//            this.currentDotOpacity.set(1, {duration : 50});
-//            this.currentDotPosition.set(position);
-//            this.index = index;
-//        }.bind(this));
+                    this.index = data.index;
+                    this.progress = progress;
+
+                    var position = _calcPosition.call(this, index, progress);
+                    currentDotPosition.set(position);
+                }.bind(this));
+                break;
+            case PaginatedLayout.TRANSITION.DISCRETE:
+                this._eventInput.on('end', function(data){
+                    this.index = data.index;
+                    this.progress = data.progress;
+                    var position = _calcPosition.call(this, this.index, this.progress);
+                    currentDotPosition.set(position);
+                    currentDotOpacity.set(0);
+                    currentDotOpacity.set(1, {duration : 100});
+                }.bind(this));
+                break;
+        }
     }
 
-    PaginatedLayout.prototype = {
-        pagesFrom : function(pages){
-            Scrollview.prototype.sequenceFrom.apply(this.scrollview, arguments);
-            for (var i = 0; i < pages.length; i++) this.addDot();
-        },
-        addDot : function(){
-            var dotModifier = new Modifier({
-                transform : Transform.translate(this.dotsWidth, 0, 0)
-            });
+    function _calcPosition(index, progress){
+        return (index + progress) * (2*this.options.dotRadius + this.options.dotSpacing);
+    }
 
-            var dot = new Surface({
-                size : [10,10],
-                properties : {
-                    borderRadius : '50%',
-                    border : '1px solid white'
-                }
-            });
-            this.dots.push(dot);
-            this.dotsWidth += 14;
-            this.dotsNode.add(dotModifier).add(dot);
-        },
-        render : function(){
-            var progress = Scrollview.prototype.getProgress.call(this.scrollview);
-            var index = this.scrollview.getCurrentIndex();
-
-            this.currentDotPosition.set((index + progress) * 14);
-
-            return [
-                this.scrollview.render(),
-                this.dotsNode.render()
-            ];
-        }
+    PaginatedLayout.TRANSITION = {
+        CONTINUOUS : 0,
+        DISCRETE : 1
     };
 
+    PaginatedLayout.DEFAULT_OPTIONS = {
+        dotRadius : 5,
+        dotColor : 'white',
+        dotSpacing : 4,
+        transition : PaginatedLayout.TRANSITION.CONTINUOUS
+    };
+
+    PaginatedLayout.prototype = Object.create(View.prototype);
+    PaginatedLayout.prototype.constructor = PaginatedLayout;
+
+    PaginatedLayout.prototype.pagesFrom = function(pages){
+        for (var i = 0; i < pages.length; i++)
+            this.addPage(pages[i]);
+    };
+
+    PaginatedLayout.prototype.addPage = function(page){
+        this.pages.push(page);
+
+        var dotModifier = new Modifier({
+            transform : Transform.translate(this.dotsWidth, 0, 0)
+        });
+
+        var dot = new Surface({
+            size : [2*this.options.dotRadius, 2*this.options.dotRadius],
+            properties : {
+                borderRadius : '50%',
+                border : '1px solid ' + this.options.dotColor
+            }
+        });
+
+        this.dotsWidth += 2*this.options.dotRadius + this.options.dotSpacing;
+        this.dotsNode.add(dotModifier).add(dot);
+    };
+
+    PaginatedLayout.prototype.getSize = function(){
+        return (this.size) ? this.size : this.pages.getSize();
+    };
+
+    PaginatedLayout.prototype.setSize = function(size){
+        this.size = size;
+    };
+
+    PaginatedLayout.prototype.getProgress = function(){
+        return this.progress;
+    };
+
+    PaginatedLayout.prototype.getCurrentIndex = function(){
+        return this.index;
+    };
+
+    PaginatedLayout.prototype.goToNextPage = function(){
+        Scrollview.prototype.goToNextPage.call(this.scrollview);
+    };
+
+    PaginatedLayout.prototype.goToPreviousPage = function(){
+        Scrollview.prototype.goToPreviousPage.call(this.scrollview);
+    };
+
+    PaginatedLayout.prototype.goToPage = function(index){
+        Scrollview.prototype.goToPage.call(this.scrollview, index);
+    };
 
     module.exports = PaginatedLayout;
 });
