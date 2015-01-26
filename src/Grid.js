828d8ce873a089338a7afa6f5914dcb32b3cd258
Joseph Orbegoso Pea <joe@trusktr.io>
2015-01-25 23:21:12 -0800

    Initial docs for Grid.

--- src/Grid.js
+++ src/Grid.js
@@ -14,7 +14,28 @@ import Molecule from './Molecule';
 
 import forLength from 'army-knife/forLength';
 
-export class Grid extends Molecule { // a scenegraph tree that lays things out in a grid. The leaf nodes are Modifiers (the cells of the grid). Put stuff in them.
+/**
+ * A scenegraph tree with a variable number of leaf node Modifiers (the grid
+ * cells) that are arranged in a grid. Add any [famous/core/RenderNode](#famous/core/RenderNode)-compatible
+ * item to each leafnode of the grid.
+ *
+ * TODO: Use Molecule instead of Modifier for the grid cells.
+ * TODO: Add an options parameter, that the Molecule constructor will handle.
+ *
+ * @class Grid
+ * @extends Molecule
+ */
+export class Grid extends Molecule {
+
+    /**
+     * Creates a new Grid having the specified number of columns, number of rows,
+     * and famo.us-style size.
+     *
+     * @constructor
+     * @param {Number} columns The integer number of columns.
+     * @param {Number} rows The integer number of rows.
+     * @param {Array} size A famo.us-style width/height size array.
+     */
     constructor(columns, rows, size) {
         super({size: size});
 
@@ -24,10 +45,16 @@ export class Grid extends Molecule { // a scenegraph tree that lays things out i
 
         if (typeof this.options.size === 'undefined') { this.setOptions({size: [undefined, undefined]}); }
 
-        forLength(this.columns*this.rows, this.createGridCell.bind(this));
+        forLength(this.columns*this.rows, this._createGridCell.bind(this));
     }
 
-    createGridCell(index) {
+    /**
+     * Creates a grid cell at the given index.
+     *
+     * @private
+     * @param {Number} index The integer index of the grid cell.
+     */
+    _createGridCell(index) {
         var column = index % this.columns;
         var row = Math.floor(index / this.columns);
 
@@ -50,10 +77,14 @@ export class Grid extends Molecule { // a scenegraph tree that lays things out i
             origin: [0.5,0.5]
         });
         // FIXME: ^^^ Why do I need an extra Modifier to align stuff in the middle of the grid cells?????
-        // TODO: Use Molecule instead of Modifier.
         this.cellNodes.push(this.add(mod).add(mod2));
     }
 
+    /**
+     * Sets the items to be layed out in the grid.
+     *
+     * @param {Array} children An array of [famous/core/RenderNode](#famous/core/RenderNode)-compatible items.
+     */
     setChildren(children) {
         forLength(this.columns*this.rows, function(index) {
             //this.cellNodes[index].set(null); // TODO: how do we erase previous children?
