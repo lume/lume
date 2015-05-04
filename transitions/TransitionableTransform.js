c9de9c7a902ffa97c20f35335c3c220cc9acf540
David Valdman <dmvaldman@gmail.com>
2015-05-03 20:27:50 -0400

    TransitionableTransform refactored for .from methods

--- transitions/TransitionableTransform.js
+++ transitions/TransitionableTransform.js
@@ -9,7 +9,6 @@
 define(function(require, exports, module) {
     var Transitionable = require('famous/core/Transitionable');
     var Transform = require('famous/core/Transform');
-    var Utility = require('famous/core/Utility');
     var EventHandler = require('famous/core/EventHandler');
 
     /**
@@ -19,175 +18,114 @@ define(function(require, exports, module) {
      * @class TransitionableTransform
      * @constructor
      *
-     * @param [transform=Transform.identity] {Transform} The initial transform state
      */
-    function TransitionableTransform(transform) {
-        this._final = Transform.identity.slice();
+    function TransitionableTransform() {
+        this._transform = Transform.identity.slice();
+        this._components = Transform.interpret(this._transform);
 
-        this._finalTranslate = [0, 0, 0];
-        this._finalRotate = [0, 0, 0];
-        this._finalSkew = [0, 0, 0];
-        this._finalScale = [1, 1, 1];
-
+        this._dirty = true;
         this._dirtyLock = 0;
-
-        this.translate = new Transitionable(this._finalTranslate);
-        this.rotate = new Transitionable(this._finalRotate);
-        this.skew = new Transitionable(this._finalSkew);
-        this.scale = new Transitionable(this._finalScale);
-
-        if (transform && transform !== Transform.identity)
-            this.set(transform);
 
         this._eventOutput = new EventHandler();
         this._eventInput = new EventHandler();
         EventHandler.setInputHandler(this, this._eventInput);
         EventHandler.setOutputHandler(this, this._eventOutput);
-        this._eventInput.bindThis(this);
-
-        this._eventInput.subscribe(this.translate);
-        this._eventInput.subscribe(this.rotate);
-        this._eventInput.subscribe(this.skew);
-        this._eventInput.subscribe(this.scale);
-
-        this._eventInput.on('dirty', function(){
-            if (this._dirtyLock == 0) this._eventOutput.emit('dirty');
-        });
-
-        this._eventInput.on('clean', function(){
-            if (this._dirtyLock == 0) this._eventOutput.emit('clean');
-        });
 
         this._eventInput.on('start', function(){
-            if (this._dirtyLock == 0) this._eventOutput.emit('start');
+            if (!this._dirty && this._dirtyLock == 0) {
+                this._dirty = true;
+                this._eventOutput.emit('start');
+            }
             this._dirtyLock++;
-        });
+        }.bind(this));
 
         this._eventInput.on('end', function(){
             this._dirtyLock--;
-            if (this._dirtyLock == 0) this._eventOutput.emit('end');
-        });
+            if (this._dirty && this._dirtyLock == 0) {
+                this._dirty = false;
+                this._eventOutput.emit('end');
+            }
+        }.bind(this));
     }
 
-    function _build() {
-        return Transform.build({
-            translate: this.translate.get(),
-            rotate: this.rotate.get(),
-            skew: this.skew.get(),
-            scale: this.scale.get()
-        });
+    TransitionableTransform.prototype.translateFrom = function translateFrom(translate) {
+        if (translate instanceof Function){
+            this._translateGetter = translate;
+            this._dirty = true;
+            this._dirtyLock++;
+        }
+        else if (translate instanceof Object && translate.get){
+            this._translateGetter = translate.get.bind(translate);
+            this._eventInput.subscribe(translate);
+        }
+        else {
+            this._translateGetter = null;
+            this._components.translate = translate;
+            this._dirty = true;
+        }
+        return this;
+    };
+
+    TransitionableTransform.prototype.scaleFrom = function scaleFrom(scale) {
+        if (scale instanceof Function){
+            this._scaleGetter = scale;
+            this._dirty = true;
+            this._dirtyLock++;
+        }
+        else if (scale instanceof Object && scale.get){
+            this._scaleGetter = scale.get.bind(scale);
+            this._eventInput.subscribe(scale);
+        }
+        else {
+            this._scaleGetter = null;
+            this._components.scale = scale;
+            this._dirty = true;
+        }
+        return this;
+    };
+
+    TransitionableTransform.prototype.rotateFrom = function rotateFrom(rotate) {
+        if (rotate instanceof Function){
+            this._rotateGetter = rotate;
+            this._dirty = true;
+            this._dirtyLock++;
+        }
+        else if (rotate instanceof Object && rotate.get){
+            this._rotateGetter = rotate.get.bind(rotate);
+            this._eventInput.subscribe(rotate);
+        }
+        else {
+            this._rotateGetter = null;
+            this._components.rotate = rotate;
+            this._dirty = true;
+        }
+        return this;
+    };
+
+    TransitionableTransform.prototype.skewFrom = function skewFrom(skew) {
+        if (skew instanceof Function){
+            this._skewGetter = skew;
+            this._dirty = true;
+            this._dirtyLock++;
+        }
+        else if (skew instanceof Object && skew.get){
+            this._skewGetter = skew.get.bind(skew);
+            this._eventInput.subscribe(skew);
+        }
+        else {
+            this._skewGetter = null;
+            this._components.rotate = skew;
+            this._dirty = true;
+        }
+        return this;
+    };
+
+    function _update(){
+        if (this._translateGetter) this._components.translate = this._translateGetter();
+        if (this._scaleGetter) this._components.scale = this._scaleGetter();
+        if (this._rotateGetter) this._components.rotate = this._rotateGetter();
+        if (this._skewGetter) this._components.skew = this._skewGetter();
     }
-
-    function _buildFinal() {
-        return Transform.build({
-            translate: this._finalTranslate,
-            rotate: this._finalRotate,
-            skew: this._finalSkew,
-            scale: this._finalScale
-        });
-    }
-
-    /**
-     * Setter for a TransitionableTransform with optional parameters to transition
-     * between Transforms
-     *
-     * @method set
-     * @chainable
-     *
-     * @param transform {Array}     New transform state
-     * @param [transition] {Object} Transition definition
-     * @param [callback] {Function} Callback
-     * @return {TransitionableTransform}
-     */
-    TransitionableTransform.prototype.set = function set(transform, transition, callback) {
-        var components = Transform.interpret(transform);
-
-        this._finalTranslate = components.translate;
-        this._finalRotate = components.rotate;
-        this._finalSkew = components.skew;
-        this._finalScale = components.scale;
-        this._final = transform;
-
-        var _callback = callback ? Utility.after(4, callback) : null;
-        this.translate.set(components.translate, transition, _callback);
-        this.rotate.set(components.rotate, transition, _callback);
-        this.skew.set(components.skew, transition, _callback);
-        this.scale.set(components.scale, transition, _callback);
-        return this;
-    };
-
-    /**
-     * An optimized way of setting only the translation component of a Transform
-     *
-     * @method setTranslate
-     * @chainable
-     *
-     * @param translate {Array}     New translation state
-     * @param [transition] {Object} Transition definition
-     * @param [callback] {Function} Callback
-     * @return {TransitionableTransform}
-     */
-    TransitionableTransform.prototype.setTranslate = function setTranslate(translate, transition, callback) {
-        this._finalTranslate = translate;
-        this._final = _buildFinal.call(this);
-        this.translate.set(translate, transition, callback);
-        return this;
-    };
-
-    /**
-     * An optimized way of setting only the scale component of a Transform
-     *
-     * @method setScale
-     * @chainable
-     *
-     * @param scale {Array}         New scale state
-     * @param [transition] {Object} Transition definition
-     * @param [callback] {Function} Callback
-     * @return {TransitionableTransform}
-     */
-    TransitionableTransform.prototype.setScale = function setScale(scale, transition, callback) {
-        this._finalScale = scale;
-        this._final = _buildFinal.call(this);
-        this.scale.set(scale, transition, callback);
-        return this;
-    };
-
-    /**
-     * An optimized way of setting only the rotational component of a Transform
-     *
-     * @method setRotate
-     * @chainable
-     *
-     * @param eulerAngles {Array}   Euler angles for new rotation state
-     * @param [transition] {Object} Transition definition
-     * @param [callback] {Function} Callback
-     * @return {TransitionableTransform}
-     */
-    TransitionableTransform.prototype.setRotate = function setRotate(eulerAngles, transition, callback) {
-        this._finalRotate = eulerAngles;
-        this._final = _buildFinal.call(this);
-        this.rotate.set(eulerAngles, transition, callback);
-        return this;
-    };
-
-    /**
-     * An optimized way of setting only the skew component of a Transform
-     *
-     * @method setSkew
-     * @chainable
-     *
-     * @param skewAngles {Array}    New skew state
-     * @param [transition] {Object} Transition definition
-     * @param [callback] {Function} Callback
-     * @return {TransitionableTransform}
-     */
-    TransitionableTransform.prototype.setSkew = function setSkew(skewAngles, transition, callback) {
-        this._finalSkew = skewAngles;
-        this._final = _buildFinal.call(this);
-        this.skew.set(skewAngles, transition, callback);
-        return this;
-    };
-
     /**
      * Getter. Returns the current state of the Transform
      *
@@ -196,38 +134,12 @@ define(function(require, exports, module) {
      * @return {Transform}
      */
     TransitionableTransform.prototype.get = function get() {
-        return this.isActive() ? _build.call(this) : this._final;
-    };
-
-    /**
-     * Determine if the TransitionalTransform is currently transitioning
-     *
-     * @method isActive
-     *
-     * @return {Boolean}
-     */
-    TransitionableTransform.prototype.isActive = function isActive() {
-        return this.translate.isActive() || this.rotate.isActive() || this.scale.isActive() || this.skew.isActive();
-    };
-
-    /**
-     * Halts the transition
-     *
-     * @method halt
-     */
-    TransitionableTransform.prototype.halt = function halt() {
-        this.translate.halt();
-        this.rotate.halt();
-        this.skew.halt();
-        this.scale.halt();
-
-        this._final = this.get();
-        this._finalTranslate = this.translate.get();
-        this._finalRotate = this.rotate.get();
-        this._finalSkew = this.skew.get();
-        this._finalScale = this.scale.get();
-
-        return this;
+        if (this._dirty){
+            _update.call(this);
+            this._transform = Transform.build(this._components);
+            if (this._dirtyLock == 0) this._dirty = false;
+        }
+        return this._transform;
     };
 
     module.exports = TransitionableTransform;
