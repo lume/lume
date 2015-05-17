cd70b4102d4b9eb56fdea4e7a1f4b2ee8a1f04b8
David Valdman <dmvaldman@gmail.com>
2015-05-16 23:35:54 -0400

    Converted HeaderFooterLayout to use resize events and setup

--- views/HeaderFooterLayout.js
+++ views/HeaderFooterLayout.js
@@ -9,10 +9,10 @@
 /* Modified work copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
-    var RenderNode = require('famous/core/RenderNode');
     var Transform = require('famous/core/Transform');
     var View = require('famous/core/View');
-    var Spec = require('famous/core/Spec');
+    var Getter = require('famous/core/GetHelper');
+    var Observable = require('famous/core/Observable');
 
     /**
      * A layout which will arrange three renderables into a header and footer area of defined size,
@@ -30,71 +30,86 @@ define(function(require, exports, module) {
     module.exports = View.extend({
         defaults : {
             direction: CONSTANTS.DIRECTION.Y,
-            headerLength: undefined,
-            footerLength: undefined,
-            defaultHeaderLength: 0,
-            defaultFooterLength: 0
+            defaultHeaderLength: Number.NaN,
+            defaultFooterLength: Number.NaN
+        },
+        events : {
+            resize : onResize
         },
         state : {
-            headerLength : Number,
-            footerLength : Number,
-            direction : Number
+            headerLength : Observable,
+            footerLength : Observable,
+            totalLength : Observable
         },
         initialize : function initialize(options){
             this.header = null;
             this.footer = null;
             this.content = null;
-
-            this.state.headerLength = options.headerLength;
-            this.state.footerLength = options.footerLength;
-            this.state.direction = options.direction;
-
-            var spec = new Spec();
-            this.add(function(size){
-                var headerLength = (this.state.headerLength !== undefined)
-                    ? this.state.headerLength
-                    : _resolveNodeLength.call(this, this.header, options.defaultHeaderLength);
-
-                var footerLength = (this.state.footerLength !== undefined)
-                    ? this.state.footerLength
-                    : _resolveNodeLength.call(this, this.footer, options.defaultFooterLength);
-
-                var contentLength = size[this.state.direction] - headerLength - footerLength;
-
-                spec.getChild(0)
-                    .setSize(_finalSize.call(this, headerLength, size))
-                    .setTarget(this.header);
-
-                spec.getChild(1)
-                    .setTransform(_outputTransform.call(this, headerLength))
-                    .setSize(_finalSize.call(this, contentLength, size))
-                    .setTarget(this.content);
-
-                spec.getChild(2)
-                    .setTransform(_outputTransform.call(this, headerLength + contentLength))
-                    .setSize(_finalSize.call(this, footerLength, size))
-                    .setTarget(this.footer);
-
-                return spec;
+        },
+        setup : function(){
+            this.header.on('resize', function(size){
+                this.state.headerLength.set(size[this.options.direction])
             }.bind(this));
+
+            this.footer.on('resize', function(size){
+                this.state.footerLength.set(size[this.options.direction])
+            }.bind(this));
+
+            this.state.headerLength.set(_resolveNodeLength.call(this, this.header, this.options.defaultHeaderLength));
+            this.state.footerLength.set(_resolveNodeLength.call(this, this.footer, this.options.defaultFooterLength));
+
+            var headerLength = new Getter(this.state.headerLength);
+            var footerLength = new Getter(this.state.footerLength);
+            var totalLength = new Getter(this.state.totalLength);
+
+            var contentLength = totalLength.map(function(totalLength){
+                return totalLength - this.state.footerLength.get() - this.state.headerLength.get();
+            }.bind(this));
+
+            var contentDisplacement = headerLength;
+
+            var footerDisplacement = totalLength.map(function(length){
+                return length - this.state.footerLength.get();
+            }.bind(this));
+
+            var headerSize = headerLength.map(_outputSize.bind(this));
+            var footerSize = footerLength.map(_outputSize.bind(this));
+            var contentSize = contentLength.map(_outputSize.bind(this));
+
+            var contentTransform = contentDisplacement.map(_outputTransform.bind(this));
+            var footerTransform = footerDisplacement.map(_outputTransform.bind(this));
+
+            this.add({size : headerSize})
+                .add(this.header);
+
+            this.add({size : contentSize, transform : contentTransform})
+                .add(this.content);
+
+            this.add({size : footerSize, transform : footerTransform})
+                .add(this.footer);
         }
     }, CONSTANTS);
 
+    function onResize(size){
+        this.state.totalLength.set(size[this.options.direction]);
+    }
+
     function _resolveNodeLength(node, defaultLength) {
-        var nodeSize = node.getSize();
-        return nodeSize ? nodeSize[this.state.direction] : defaultLength;
+        return (defaultLength)
+            ? defaultLength
+            : node.getSize()[this.options.direction];
     }
 
     function _outputTransform(offset) {
-        return (this.state.direction === CONSTANTS.DIRECTION.X)
+        return (this.options.direction === CONSTANTS.DIRECTION.X)
             ? Transform.translate(offset, 0, 0)
             : Transform.translate(0, offset, 0);
     }
 
-    function _finalSize(directionSize, size) {
-        return (this.state.direction === CONSTANTS.DIRECTION.X)
-            ? [directionSize, size[1]]
-            : [size[0], directionSize];
+    function _outputSize(length) {
+        return (this.options.direction === CONSTANTS.DIRECTION.X)
+            ? [length, undefined]
+            : [undefined, length];
     }
 
 });
