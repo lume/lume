4d097a8a165daae970c035a882dbda59eed43d31
David Valdman <dmvaldman@gmail.com>
2015-06-04 11:01:47 -0700

    Simplified dirtyQueue

--- core/dirtyQueue.js
+++ core/dirtyQueue.js
@@ -1,20 +1,5 @@
 /* copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
-    var queue = [];
-    var dirtyQueue = {};
-
-    dirtyQueue.push = function(obj){
-        if (queue.indexOf(obj) !== -1) return;
-        Array.prototype.push.apply(queue, arguments);
-    };
-
-    dirtyQueue.flush = function(){
-        var L = queue.length;
-        for (var i = 0; i < L; i++){
-            (queue.shift()).clean();
-        }
-    };
-
-    module.exports = dirtyQueue;
+    module.exports = [];
 });
