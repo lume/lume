9d82212898d7062ad812b04813177ecf5c567c5d
David Valdman <dmvaldman@gmail.com>
2015-06-12 17:24:47 -0700

    ModifierStream dirtying

--- core/ModifierStream.js
+++ core/ModifierStream.js
@@ -3,10 +3,19 @@ define(function(require, exports, module) {
     var Stream = require('famous/streams/Stream');
     var Observable = require('famous/core/Observable');
     var postTickQueue = require('famous/core/postTickQueue');
+    var dirtyObjects = require('famous/core/dirtyObjects');
 
     function Modifier(sources) {
         this.stream = this.createStream(sources);
         EventHandler.setOutputHandler(this, this.stream);
+
+        this.on('start', function(){
+            dirtyObjects.trigger('dirty');
+        });
+
+        this.on('end', function(){
+            dirtyObjects.trigger('clean');
+        });
     }
 
     Modifier.prototype.createStream = function (sources){
