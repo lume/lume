aeff3d2975a9c257e85579ce5c6510c8b83054ab
David Valdman <dmvaldman@gmail.com>
2015-06-21 18:37:04 -0700

    Add SizeStream

---
+++ streams/SizeStream.js
@@ -0,0 +1,46 @@
+define(function(require, exports, module) {
+    var EventHandler = require('famous/core/EventHandler');
+    var Timer = require('famous/utilities/Timer');
+    var dirtyQueue = require('famous/core/queues/dirtyQueue');
+    var Stream = require('famous/streams/Stream');
+
+    var DEBOUNCE_FRAME_LIMIT = 10;
+
+    function SizeStream(){
+        this._eventInput = new EventHandler();
+        this._eventOutput = new EventHandler();
+        EventHandler.setInputHandler(this, this._eventInput);
+        EventHandler.setOutputHandler(this, this._eventOutput);
+
+        var resizeFired = false;
+
+        var debouncedResize = Timer.frameDebounce(function(size){
+            dirtyQueue.push(function(){
+                this.emit('end', size);
+                resizeFired = false;
+            }.bind(this));
+        }.bind(this), DEBOUNCE_FRAME_LIMIT);
+
+        //TODO: last resize should win if multiple are fired
+        this._eventInput.on('resize', function(size){
+
+            if (!resizeFired) {
+                this.emit('start', size);
+
+                resizeFired = true;
+                debouncedResize(size);
+            }
+            else {
+                this.emit('update', size);
+                debouncedResize(size);
+            }
+            this.emit('resize', size);
+
+        }.bind(this));
+    }
+
+    SizeStream.prototype = Object.create(Stream.prototype);
+    SizeStream.prototype.constructor = SizeStream;
+
+    module.exports = SizeStream;
+});
