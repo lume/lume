dac97a3f034ac0506675dfaeea68237ddee7e79a
David Valdman <dmvaldman@gmail.com>
2015-07-28 09:40:18 -0700

    DirtyObjects is an EventEmitter

--- core/dirtyObjects.js
+++ core/dirtyObjects.js
@@ -1,16 +1,6 @@
 /* copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
-    var EventHandler = require('famous/core/EventHandler');
-    var dirtyQueue = require('famous/core/queues/dirtyQueue');
-
-    var eventOutput = new EventHandler();
-
-    var dirtyObjects = {};
-
-    dirtyObjects.trigger = eventOutput.emit.bind(eventOutput);
-
-    dirtyObjects.on = eventOutput.on.bind(eventOutput);
-
-    module.exports = dirtyObjects;
+    var EventEmitter = require('famous/core/EventEmitter');
+    module.exports = new EventEmitter();
 });
