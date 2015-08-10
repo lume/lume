f620cf6655f7b926a119ff0957befdec1745ce1b
David Valdman <dmvaldman@gmail.com>
2015-08-09 17:58:07 -0700

    Update AccordionLayout for pivoting generalization

--- views/AccordionLayout.js
+++ views/AccordionLayout.js
@@ -25,25 +25,32 @@ define(function(require, exports, module) {
     var AccordionLayout = View.extend({
         defaults : {
             direction : CONSTANTS.DIRECTION.Y,
-            initialAngles : []
+            initialAngles : [],
+            pivotIndex: 0,
+            pivotOffset: 0,
+            pivotOffsetRatio: 0
         },
         initialize : function(options){
             this.angles = new Transitionable(options.initialAngles);
             this.input = new Stream();
-            this.maxLength = 0;
+            this.output = new Stream();
+            this.output.subscribe(this._eventOutput);
+
+            this.maxLength = 0; // length of all nodes fully extended
+            this.offset = 0; // shift accordion so starting point is at (0,0)
 
             var displacement = 0;
             this.currentAngles = Stream.lift(function(input, angles){
                 if (input === undefined) return angles;
 
-                displacement += input.delta;
+                displacement -= input.delta;
 
                 var ratio = ((this.maxLength - displacement) / this.maxLength);
 
                 var currentAngles = [];
                 for (var i = 0; i < angles.length; i++){
                     var angle = angles[i];
-                    var currentAngle = (ratio * angle) % (2*Math.PI);
+                    var currentAngle = (ratio * angle) % (2 * Math.PI);
                     currentAngles.push(currentAngle);
                 }
 
@@ -53,20 +60,62 @@ define(function(require, exports, module) {
         setNodes : function(nodes){
             var transforms = this.currentAngles.map(function(angles){
                 var transforms = [];
-                var y = 0;
-                var z = 0;
-                for (var i = 0; i < nodes.length; i++){
-                    var angle = angles[i];
-                    var l = nodes[i].getSize()[1];
 
-                    var transform = (this.options.direction === CONSTANTS.DIRECTION.Y)
+                var direction = this.options.direction;
+                var startIndex = this.options.pivotIndex;
+
+                var pivotOffset = (this.options.pivotOffset)
+                    ? this.options.pivotOffset
+                    : this.options.pivotOffsetRatio * nodes[startIndex].getSize()[direction];
+
+                var startAngle = angles[startIndex];
+                var originY = -pivotOffset * Math.cos(startAngle);
+                var originZ = -pivotOffset * Math.sin(startAngle);
+
+                var accordionOffset = this.offset;    // shift to ensure starting point is at 0
+                var y = originY - accordionOffset;
+                var z = originZ;
+
+                // TODO: remove these offsets
+                var offsetX = 100;
+                var offsetY = 200;
+
+                var angle, transform, l;
+                for (i = startIndex - 1; i >= 0; i--) {
+                    angle = angles[i];
+                    l = nodes[i].getSize()[direction];
+
+                    y -= l * Math.cos(angle);
+                    z -= l * Math.sin(angle);
+
+                    transform = (direction === CONSTANTS.DIRECTION.Y)
                         ? Transform.thenMove(
                             Transform.rotateX(angle),
-                            [300, y+200, z]
+                            [offsetX, y + offsetY, z]
                         )
                         : Transform.thenMove(
-                            Transform.rotateY(angle),
-                            [y, 0, z]
+                            Transform.rotateY(-angle),
+                            [offsetX + y, offsetY, z]
+                        );
+
+                    transforms[i] = transform;
+                }
+
+                y = originY - accordionOffset;
+                z = originZ;
+
+                for (i = startIndex; i < nodes.length; i++){
+                    angle = angles[i];
+                    l = nodes[i].getSize()[direction];
+
+                    transform = (direction === CONSTANTS.DIRECTION.Y)
+                        ? Transform.thenMove(
+                            Transform.rotateX(angle),
+                            [offsetX, y + offsetY, z]
+                        )
+                        : Transform.thenMove(
+                            Transform.rotateY(-angle),
+                            [offsetX + y, offsetY, z]
                         );
 
                     z += l * Math.sin(angle);
@@ -80,8 +129,27 @@ define(function(require, exports, module) {
                 };
             }.bind(this));
 
-            for (var i = 0; i < nodes.length; i++)
+            this.offset = 0;
+            this.startLength = 0;
+            for (var i = 0; i < nodes.length; i++){
+                var angle = this.options.initialAngles[i];
+                var l = nodes[i].getSize()[this.options.direction];
+                var length = l * Math.cos(angle);
+                if (i < this.options.pivotIndex)
+                    this.offset -= length;
+
+                this.startLength += length;
+
+                var layout = new LayoutNode({
+                    transform : transforms.pluck('transforms').pluck(i)
+                });
+
+                this.add(layout).add(nodes[i]);
+
                 this.maxLength += nodes[i].getSize()[this.options.direction];
+            }
+
+            console.log(this.startLength)
 
             this._eventOutput.subscribe(transforms
                 .pluck('length')
@@ -92,13 +160,6 @@ define(function(require, exports, module) {
                     };
                 }.bind(this))
             );
-
-            for (var i = 0; i < nodes.length; i++){
-                var layout = new LayoutNode({
-                    transform : transforms.pluck('transforms').pluck(i)
-                });
-                this.add(layout).add(nodes[i])
-            }
         },
         setAngles : function(angles, transition, callback){
             this.angles.set(angles, transition, callback)
