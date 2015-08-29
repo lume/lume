d61b21ba30d0b5170064719a65030c25cfbe08f8
David Valdman <dmvaldman@gmail.com>
2015-08-29 12:52:46 +0200

    ScrollInput uses Timer to debouce  event. Cleanups

--- inputs/ScrollInput.js
+++ inputs/ScrollInput.js
@@ -9,39 +9,29 @@
 /* Modified work copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
-    var EventHandler = require('../core/EventHandler');
-    var Engine = require('../core/Engine');
-    var OptionsManager = require('../core/OptionsManager');
+    var EventHandler = require('samsara/core/EventHandler');
+    var OptionsManager = require('samsara/core/OptionsManager');
+    var dirtyQueue = require('samsara/core/queues/dirtyQueue');
+    var postTickQueue = require('samsara/core/queues/postTickQueue');
+    var Timer = require('samsara/core/Timer');
 
     /**
-     * Handles piped in mousewheel events.
+     * Handles DOM wheel/mousewheel events.
      *   Emits 'start', 'update', and 'end' events with payloads including:
-     *   delta: change since last position,
-     *   position: accumulated deltas,
+     *   delta: pixel differential since last event,
+     *   value: pixel displacement,
      *   velocity: speed of change in pixels per ms,
-     *
-     *   Can be used as delegate of GenericInput.
      *
      * @class ScrollInput
      * @constructor
-     * @param {Object} [options] overrides of default options
-     * @param {Number} [options.direction] Pay attention to x changes (ScrollInput.DIRECTION_X),
-     *   y changes (ScrollInput.DIRECTION_Y) or both (undefined)
-     * @param {Number} [options.minimumEndSpeed] End speed calculation floors at this number, in pixels per ms
-     * @param {boolean} [options.rails] whether to snap position calculations to nearest axis
-     * @param {Number | Array.Number} [options.scale] scale outputs in by scalar or pair of scalars
-     * @param {Number} [options.stallTime] reset time for velocity calculation in ms
      */
     function ScrollInput(options) {
-        this.options = Object.create(ScrollInput.DEFAULT_OPTIONS);
-        this._optionsManager = new OptionsManager(this.options);
-        if (options) this.setOptions(options);
+        this.options = OptionsManager.setOptions(this, options);
 
         this._payload = {
             delta    : null,
-            position : null,
-            velocity : null,
-            scroll   : true
+            value    : null,
+            velocity : null
         };
 
         this._eventInput = new EventHandler();
@@ -50,137 +40,96 @@ define(function(require, exports, module) {
         EventHandler.setInputHandler(this, this._eventInput);
         EventHandler.setOutputHandler(this, this._eventOutput);
 
-        this._position = (this.options.direction === undefined) ? [0,0] : 0;
-        this._prevTime = undefined;
-        this._boundNewFrame = _newFrame.bind(this);
         this._eventInput.on('mousewheel', _handleMove.bind(this));
         this._eventInput.on('wheel', _handleMove.bind(this));
+
+        this._value = (this.options.direction === undefined) ? [0,0] : 0;
+        this._prevTime = undefined;
         this._inProgress = false;
-        this._loopBound = false;
+
+        var self = this;
+        this._scrollEnd = Timer.debounce(function(){
+            self._inProgress = false;
+            dirtyQueue.push(function(){
+                self._eventOutput.emit('end', self._payload);
+            });
+        }, 100);
     }
 
     ScrollInput.DEFAULT_OPTIONS = {
         direction: undefined,
-        minimumEndSpeed: 1e-1,
-        rails: false,
-        scale: 1,
-        stallTime: 80,
-        lineHeight: 40,
-        preventDefault: true
+        scale: 1
     };
 
-    ScrollInput.DIRECTION_X = 0;
-    ScrollInput.DIRECTION_Y = 1;
+    ScrollInput.DIRECTION = {
+        X : 0,
+        Y : 1
+    };
 
     var MINIMUM_TICK_TIME = 8;
 
     var _now = Date.now;
 
-    function _newFrame() {
-        var dt = _now() - this._prevTime;
-        if (this._inProgress && dt > this.options.stallTime) {
-//            this._payload.velocity = 0;
-            this._eventOutput.emit('end', this._payload);
-
-            this._inProgress = false;
-            this._loopBound = false;
-            Engine.off('prerender', this._boundNewFrame);
-        }
-    }
-
     function _handleMove(event) {
-        if (this.options.preventDefault) event.preventDefault();
-
         if (!this._inProgress) {
-            if (!this._loopBound) {
-                Engine.on('prerender', this._boundNewFrame);
-                this._loopBound = true;
-            }
-
-            this._position = (this.options.direction === undefined) ? [0,0] : 0;
+            this._value = (this.options.direction === undefined) ? [0,0] : 0;
             payload = this._payload;
-            payload.position = this._position;
+            payload.value = this._value;
             payload.clientX = event.clientX;
             payload.clientY = event.clientY;
             payload.offsetX = event.offsetX;
             payload.offsetY = event.offsetY;
 
             this._eventOutput.emit('start', payload);
+            this._inProgress = true;
+            return;
         }
 
-        this._inProgress = true;
         var currTime = _now();
         var prevTime = this._prevTime || currTime;
 
         var diffX = (event.wheelDeltaX !== undefined) ? event.wheelDeltaX : -event.deltaX;
         var diffY = (event.wheelDeltaY !== undefined) ? event.wheelDeltaY : -event.deltaY;
 
-        if (this.options.rails) {
-            if (Math.abs(diffX) > Math.abs(diffY)) diffY = 0;
-            else diffX = 0;
-        }
-
-        var diffTime = Math.max(currTime - prevTime, MINIMUM_TICK_TIME); // minimum tick time
+        var invDeltaT = 1 / Math.max(currTime - prevTime, MINIMUM_TICK_TIME); // minimum tick time
         this._prevTime = currTime;
 
-        var velX = diffX / diffTime;
-        var velY = diffY / diffTime;
+        var velX = diffX * invDeltaT;
+        var velY = diffY * invDeltaT;
 
         var scale = this.options.scale;
         var nextVel;
         var nextDelta;
 
-        if (this.options.direction === ScrollInput.DIRECTION_X) {
+        if (this.options.direction === ScrollInput.DIRECTION.X) {
             nextDelta = scale * diffX;
             nextVel = scale * velX;
-            this._position += nextDelta;
+            this._value += nextDelta;
         }
-        else if (this.options.direction === ScrollInput.DIRECTION_Y) {
+        else if (this.options.direction === ScrollInput.DIRECTION.Y) {
             nextDelta = scale * diffY;
             nextVel = scale * velY;
-            this._position += nextDelta;
+            this._value += nextDelta;
         }
         else {
             nextDelta = [scale * diffX, scale * diffY];
             nextVel = [scale * velX, scale * velY];
-            this._position[0] += nextDelta[0];
-            this._position[1] += nextDelta[1];
+            this._value[0] += nextDelta[0];
+            this._value[1] += nextDelta[1];
         }
 
         var payload = this._payload;
         payload.delta    = nextDelta;
         payload.velocity = nextVel;
-        payload.position = this._position;
+        payload.value = this._value;
 
-        this._eventOutput.emit('update', payload);
+        postTickQueue.push(function(){
+            this._eventOutput.emit('update', payload);
+        }.bind(this));
+
+        // debounce `end` event
+        this._scrollEnd();
     }
-
-    /**
-     * Return entire options dictionary, including defaults.
-     *
-     * @method getOptions
-     * @return {Object} configuration options
-     */
-    ScrollInput.prototype.getOptions = function getOptions() {
-        return this.options;
-    };
-
-    /**
-     * Set internal options, overriding any default options
-     *
-     * @method setOptions
-     *
-     * @param {Object} [options] overrides of default options
-     * @param {Number} [options.minimimEndSpeed] If final velocity smaller than this, round down to 0.
-     * @param {Number} [options.stallTime] ms of non-motion before 'end' emitted
-     * @param {Number} [options.rails] whether to constrain to nearest axis.
-     * @param {Number} [options.direction] ScrollInput.DIRECTION_X, DIRECTION_Y -
-     *    pay attention to one specific direction.
-     * @param {Number} [options.scale] constant factor to scale velocity output
-     */
-    ScrollInput.prototype.setOptions = function setOptions(options) {
-        return this._optionsManager.setOptions(options);
-    };
 
     module.exports = ScrollInput;
 });
