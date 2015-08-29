a37d93eef529dafffaaf30a74c1f0f467d82a532
David Valdman <dmvaldman@gmail.com>
2015-08-29 12:28:09 +0200

    Observable no longer emits Resize events

--- core/Observable.js
+++ core/Observable.js
@@ -1,16 +1,16 @@
 define(function(require, exports, module) {
-    var Stream = require('samsara/streams/Stream');
+    var SimpleStream = require('samsara/streams/SimpleStream');
     var preTickQueue = require('samsara/core/queues/preTickQueue');
     var dirtyQueue = require('samsara/core/queues/dirtyQueue');
 
     function Observable(value){
-        Stream.call(this);
+        SimpleStream.call(this);
         this.value = value;
 
         if (value !== undefined) this.set(value);
     }
 
-    Observable.prototype = Object.create(Stream.prototype);
+    Observable.prototype = Object.create(SimpleStream.prototype);
     Observable.prototype.constructor = Observable;
 
     Observable.prototype.get = function(){
@@ -22,11 +22,7 @@ define(function(require, exports, module) {
             this.value = value;
             this.emit('start', value);
 
-            // if listening to resize
-            this.emit('resize', value);
-
             dirtyQueue.push(function(){
-//                this.emit('resize', value);
                 this.emit('end', value);
             }.bind(this));
         }.bind(this))
