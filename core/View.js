87f35fe28240ca48e07d0065fe39cca278302ad8
David Valdman <dmvaldman@gmail.com>
2015-08-30 22:02:58 +0200

    View has origin and size options

--- core/View.js
+++ core/View.js
@@ -11,9 +11,17 @@
 define(function(require, exports, module) {
     var SceneGraphNode = require('samsara/core/nodes/SceneGraphNode');
     var Controller = require('samsara/core/Controller');
+    var SizeNode = require('samsara/core/nodes/SizeNode');
     var LayoutNode = require('samsara/core/nodes/LayoutNode');
     var Transitionable = require('samsara/core/Transitionable');
     var EventHandler = require('samsara/core/EventHandler');
+    var EventMapper = require('samsara/events/EventMapper');
+
+    var Stream = require('samsara/streams/Stream');
+    var ResizeStream = require('samsara/streams/ResizeStream');
+    var SizeObservable = require('samsara/core/SizeObservable');
+    var layoutAlgebra = require('samsara/core/algebras/layout');
+    var sizeAlgebra = require('samsara/core/algebras/size');
 
     /**
      * @class View
@@ -27,19 +35,44 @@ define(function(require, exports, module) {
             origin : null
         },
         events : null,
-        constructor : function View(){
-            this._node = new SceneGraphNode();
-            this._node.tempRoot = this._node;
-
+        constructor : function View(options){
             this.size = new EventHandler();
             this.layout = new EventHandler();
 
+            this.sizeObserver = new SizeObservable(options.size);
+
+            this._node = new SceneGraphNode();
+            this._node.tempRoot = this._node;
+
             Controller.apply(this, arguments);
 
-            this._eventInput.subscribe(this._optionsManager);
+            var sizeMapper = ResizeStream.lift(
+                function SGSizeAlgebra (size, parentSize){
+                    return (size)
+                        ? sizeAlgebra({size : size}, parentSize)
+                        : parentSize;
+                },
+                [this.sizeObserver, this.size]
+            );
 
-            this._node.layout.subscribe(this.layout);
-            this._node.size.subscribe(this.size);
+            this._node.size.subscribe(sizeMapper).subscribe(this.size);
+
+            if (options.origin){
+                var layoutMapper = Stream.lift(
+                    function ViewLayoutAlgebra (parentSpec, size){
+                        if (!parentSpec || !size) return;
+                        return (this.options.origin)
+                            ? layoutAlgebra({origin : this.options.origin}, parentSpec, size)
+                            : parentSpec;
+                    }.bind(this),
+                    [this.layout, sizeMapper || this.size]
+                );
+
+                this._node.layout.subscribe(layoutMapper).subscribe(this.layout);
+            }
+            else this._node.layout.subscribe(this.layout);
+
+            this._eventInput.subscribe(this._optionsManager);
         },
         set : function set(){
             return SceneGraphNode.prototype.set.apply(this._node, arguments);
@@ -47,30 +80,13 @@ define(function(require, exports, module) {
         add : function add(){
             return SceneGraphNode.prototype.add.apply(this._node, arguments);
         },
-        getSize : function getSize(){
-            //TODO: if given true size, then pass down to this._node.getSize
-            return (this.options.size)
-                ? this.options.size
-                : this._node.getSize();
-        },
         setSize : function setSize(size){
-            if (this.options.size === size) return;
-            if (!this._modifier){
-                this._modifier = new LayoutNode({size : size});
-                this.set(this._modifier);
-            }
-            else this._modifier.addStream({size : size});
-            return this;
+            //TODO: make this dynamic
+            this.sizeObserver.set(size);
         },
         setOrigin : function setOrigin(origin){
-            if (this.options.origin === origin) return;
+            //TODO: make this dynamic
             this.options.origin = origin;
-            if (!this._modifier){
-                this._modifier = new LayoutNode({origin : origin});
-                this.set(this._modifier);
-            }
-            else this._modifier.addStream({origin : origin});
-            return this;
         }
     });
 
