6030cba6931d58ecd2f651819c887a89f549610b
David Valdman <dmvaldman@gmail.com>
2015-09-02 11:45:08 +0200

    Simplify ResizeStream by removing state dependency

--- streams/ResizeStream.js
+++ streams/ResizeStream.js
@@ -2,56 +2,20 @@ define(function(require, exports, module) {
     var SimpleStream = require('samsara/streams/SimpleStream');
     var EventMapper = require('samsara/events/EventMapper');
     var EventHandler = require('samsara/core/EventHandler');
-    var dirtyObjects = require('samsara/core/dirtyObjects');
-
-    var preTickQueue = require('samsara/core/queues/preTickQueue');
-    var postTickQueue = require('samsara/core/queues/postTickQueue');
-    var dirtyQueue = require('samsara/core/queues/dirtyQueue');
-    var State = require('samsara/core/SUE');
 
     var EVENTS = {
-        START : 'start',
-        END : 'end',
         RESIZE : 'resize'
     };
 
     function ResizeStream(){
-        var batchCount = 0; // progress of firings in each round of start/update/end
-        var batchTotal = 0; // total firings in each round of start/update/end
-
         this._eventInput = new EventHandler();
         this._eventOutput = new EventHandler();
         EventHandler.setInputHandler(this, this._eventInput);
         EventHandler.setOutputHandler(this, this._eventOutput);
 
-        var self = this;
-
         this._eventInput.on(EVENTS.RESIZE, function(data){
-            var queue;
-            switch (State.get()){
-                case State.STATES.START:
-                    queue = preTickQueue;
-                    break;
-                case State.STATES.UPDATE:
-                    queue = postTickQueue;
-                    break;
-                case State.STATES.END:
-                    queue = dirtyQueue;
-                    break;
-            }
-
-            batchCount++;
-            batchTotal++;
-            (function(count){
-                queue.push(function(){
-                    if (count == batchTotal){
-                        self._eventOutput.emit(EVENTS.RESIZE, data);
-                        batchCount = 0;
-                        batchTotal = 0;
-                    }
-                });
-            })(batchCount)
-        });
+            this._eventOutput.emit(EVENTS.RESIZE, data);
+        }.bind(this));
     }
 
     ResizeStream.prototype = Object.create(SimpleStream.prototype);
