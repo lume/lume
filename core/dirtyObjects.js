8135f472c90c79a6b3ca8d7a1fff186e8eb3a829
David Valdman <dmvaldman@gmail.com>
2015-09-06 22:47:48 +0200

    Delete dirtyObjects module

--- core/dirtyObjects.js
+++
@@ -1,6 +0,0 @@
-/* copyright © 2015 David Valdman */
-
-define(function(require, exports, module) {
-    var EventEmitter = require('samsara/core/EventEmitter');
-    module.exports = new EventEmitter();
-});
