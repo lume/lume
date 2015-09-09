20844bd0c59869c406754252617b7362cf514f2a
David Valdman <dmvaldman@gmail.com>
2015-09-10 00:34:50 +0200

    Removed trailing comma

--- streams/Accumulator.js
+++ streams/Accumulator.js
@@ -7,7 +7,7 @@ define(function(require, exports, module){
         Stream.call(this, {
             start : function(){ return this.sum || 0; }.bind(this),
             update : function(){ return this.sum; }.bind(this),
-            end : function(){ return this.sum || 0; }.bind(this),
+            end : function(){ return this.sum || 0; }.bind(this)
         });
 
         this._eventInput.on('start', function(value){
