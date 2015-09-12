ad42563ec2ca6e2ba9b0394e6fa71417eeb8969c
David Valdman <dmvaldman@gmail.com>
2015-09-12 17:24:27 +0200

    Made createStream a private function in SizeNode

--- core/nodes/SizeNode.js
+++ core/nodes/SizeNode.js
@@ -7,7 +7,8 @@ define(function(require, exports, module) {
     var SizeObservable = require('samsara/core/SizeObservable');
 
     function SizeNode(sources) {
-        this.stream = this.createStream(sources);
+        this.stream = _createStream(sources);
+        EventHandler.setOutputHandler(this, this.stream);
 
         this.stream._eventInput.on('start', function(data){
             this.stream.trigger('resize', data);
@@ -20,21 +21,7 @@ define(function(require, exports, module) {
         this.stream._eventInput.on('end', function(data){
             this.stream.trigger('resize', data);
         }.bind(this));
-
-        EventHandler.setInputHandler(this, this.stream);
-        EventHandler.setOutputHandler(this, this.stream);
     }
-
-    SizeNode.prototype.createStream = function (sources){
-        for (var key in sources){
-            var value = sources[key];
-            if (typeof value == 'number' || value instanceof Array){
-                var source = new SizeObservable(value);
-                sources[key] = source;
-            }
-        }
-        return ResizeStream.merge(sources);
-    };
 
     SizeNode.prototype.set = function(obj){
         for (var key in obj){
@@ -47,6 +34,17 @@ define(function(require, exports, module) {
             this.stream.addStream(key, source);
         }
     };
+
+    function _createStream(sources){
+        for (var key in sources){
+            var value = sources[key];
+            if (typeof value == 'number' || value instanceof Array){
+                var source = new SizeObservable(value);
+                sources[key] = source;
+            }
+        }
+        return ResizeStream.merge(sources);
+    }
 
     module.exports = SizeNode;
 });
