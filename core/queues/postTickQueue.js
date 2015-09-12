c1869b8bb3f73572b38c01d529788023437857e2
David Valdman <dmvaldman@gmail.com>
2015-09-12 17:34:35 +0200

    Documentation for queues

--- core/queues/postTickQueue.js
+++ core/queues/postTickQueue.js
@@ -1,5 +1,11 @@
 /* copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
+    /**
+     * Queue that batches `update` events.
+     *  This queue is traversed after the `preTickQueue` but before `dirtQueue`
+     *  by the Engine.
+     */
+
     module.exports = [];
 });
--- core/queues/tickQueue.js
+++ core/queues/tickQueue.js
@@ -1,5 +1,11 @@
 /* copyright © 2015 David Valdman */
 
 define(function(require, exports, module) {
+    /**
+     * This queue is executed before the postTickQueue and after the preTickQueue.
+     *  however, it differs in that the Engine does not clear the queue.
+     *  This must be done manually.
+     */
+
     module.exports = [];
 });
