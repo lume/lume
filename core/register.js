0a43bf70512243b37253563e29c5e92332cecd19
David Valdman <dmvaldman@gmail.com>
2015-09-14 20:43:45 +0200

    Documentation for register

--- core/register.js
+++ core/register.js
@@ -1,5 +1,12 @@
 define(function(require, exports, module) {
     var counter = 0;
+
+    /**
+     * Register provides a unique Id for every commitable object for later reference.
+     *
+     * @class Engine
+     * @private
+     */
     module.exports = function register(commitable){
         commitable._id = counter;
         return counter++;
