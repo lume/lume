dcd3ee47c2c9b019524d791b7339e2918d32622f
Joseph Orbegoso Pea <joe@trusktr.io>
2015-09-27 18:14:08 -0700

    Add CSS loading to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 
 module.exports = {
     resolve: {
-        extensions: [ "", ".js", ".jsx", ".glsl" ]
+        extensions: [ "", ".js", ".jsx", ".glsl", '.css']
     },
     module: {
         loaders: [
             { test: /\.jsx?$/,  loader: 'babel', exclude: /node_modules/ },
+            { test: /\.css$/, loader: 'style!css' },
             { test: /\.(glsl|frag|vert)$/, loader: 'raw' },
             { test: /\.(glsl|frag|vert)$/, loader: 'glslify' },
         ],
