bed8cf89b4c3d1e2d1b770530249ecac6fcd0097
David Valdman <dmvaldman@gmail.com>
2015-10-14 00:11:19 -0400

    Update documentation

--- core/Context.js
+++ core/Context.js
@@ -32,11 +32,12 @@ define(function(require, exports, module) {
      * @constructor
      * @namespace Core
      * @uses Core.RootNode
-     * @param container {Node} DOM element which will serve as a container for added nodes.
+     * @param [options] {Object}    Options
+     * @param [options.el] {Node}   DOM element which will serve as a container for added nodes
      */
     function Context(options) {
         options = options || {};
-        container = options.el || document.createElement(elementType);
+        var container = options.el || document.createElement(elementType);
         container.classList.add(elementClass);
 
         var allocator = new ElementAllocator(container);
@@ -46,7 +47,7 @@ define(function(require, exports, module) {
 
         this.size = this._size.map(function(size){
             return (options.el)
-                ? [this.container.clientWidth, this.container.clientHeight]
+                ? [container.clientWidth, container.clientHeight]
                 : size;
         }.bind(this));
 
