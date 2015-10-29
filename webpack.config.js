cf09f1be41c400e7169eae8a274437f881cfaa15
David Valdman <dmvaldman@gmail.com>
2015-10-29 17:43:34 -0400

    Add webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "samsara.js",
+        library: "Samsara",
+        libraryTarget: "umd"
+    }
+};
