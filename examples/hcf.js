8089ee165cc69977d0130340da1c0020eb62634f
Steve Belovarich <steve@installationspace.com>
2015-11-13 23:16:45 -0800

    Added example for an app with header, footer, aside, and main content

--- examples/hcf.js
+++ examples/hcf.js
@@ -10,20 +10,32 @@ var nodes = [{
     id: 'app-header',
     opacity : 1.0
 },
-// {
-//     position: 'absolute',
-//     origin : [0.0,0.0,0.0],
-//     align : [0.0,0.0,0.0],
-//     size : [1.0,0.8,0],
-//     scale : [1.0,1.0,1.0],
-//     rotate: [0,0,0],
-//     id: 'app-content',
-//     opacity : 1.0
-// },
 {
     position: 'absolute',
     origin : [0.0,0.0,0.0],
-    align : [0.0,1.0,0.0],
+    align : [0.0,0.1,0.0],
+    translate: [0,0,1],
+    size : [0.3336,0.8,0],
+    scale : [1.0,1.0,1.0],
+    rotate: [0,0,0],
+    id: 'app-sidebar',
+    opacity : 1.0
+},
+{
+    position: 'absolute',
+    origin : [0.0,0.0,0.0],
+    align : [0.3336,0.1,0.0],
+    translate: [0,0,1],
+    size : [0.6667,0.8,0],
+    scale : [1.0,1.0,1.0],
+    rotate: [0,0,0],
+    id: 'app-content',
+    opacity : 1.0
+},
+{
+    position: 'absolute',
+    origin : [0.0,1.0,0.0],
+    align : [0.0,0.9,0.0],
     size : [1.0,0.1,0],
     scale : [1.0,1.0,1.0],
     rotate: [0,0,0],
