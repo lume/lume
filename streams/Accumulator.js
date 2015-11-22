2cb717951501475b18b3e6c5a2144ab39d36a1c6
David Valdman <dmvaldman@gmail.com>
2015-11-21 21:49:35 -0500

    Accumulator takes clamping options

--- streams/Accumulator.js
+++ streams/Accumulator.js
@@ -1,6 +1,7 @@
 /* Copyright © 2015 David Valdman */
 
 define(function(require, exports, module){
+    var OptionsManager = require('../core/OptionsManager');
     var Stream = require('../streams/Stream');
     var preTickQueue = require('../core/queues/preTickQueue');
     var dirtyQueue = require('../core/queues/dirtyQueue');
@@ -13,7 +14,7 @@ define(function(require, exports, module){
      *
      *  @example
      *
-     *      var accumulator = new Accumulator();
+     *      var accumulator = new Accumulator(0);
      *
      *      // this gives the total displacement of mouse input
      *      accumulator.subscribe(mouseInput.pluck('delta'));
@@ -24,45 +25,61 @@ define(function(require, exports, module){
      * @namespace Streams
      * @constructor
      * @param [sum] {Number|Array}    Initial value
+     * @param [options] {Object}      Options
+     * @param [options.min] {Number}  Set a minimum value
+     * @param [options.max] {Number}  Set a maximum value
      */
-    function Accumulator(sum){
+    function Accumulator(sum, options){
+        this.options = OptionsManager.setOptions(this, options);
+
         // TODO: is this state necessary?
         this.sum = undefined;
 
         if (sum !== undefined) this.set(sum);
 
         Stream.call(this, {
-            start : function(){
-                return this.sum || 0;
-            }.bind(this),
+            start : function(){ return this.sum || 0; }.bind(this),
             update : function(){ return this.sum; }.bind(this),
             end : function(){ return this.sum || 0; }.bind(this)
         });
 
+        // TODO: is `start` event necessary?
         this._eventInput.on('start', function(value){
-            if (this.sum === undefined) {
-                value = value || 0;
-                if (value instanceof Array){
-                    this.sum = [];
-                    for (var i = 0; i < value.length; i++)
-                        this.sum[i] = value[i];
-                }
-                else this.sum = value;
+            if (this.sum !== undefined) return;
+            if (value instanceof Array) {
+                this.sum = [];
+                for (var i = 0; i < value.length; i++)
+                    this.sum[i] = clamp(value[i], this.options.min, this.options.max);
             }
+            else this.sum = clamp(value, this.options.min, this.options.max);
         }.bind(this));
 
-        this._eventInput.on('update', function(value){
-            if (value instanceof Array){
+        this._eventInput.on('update', function(delta){
+            if (delta instanceof Array){
                 if (!this.sum) this.sum = [];
-                for (var i = 0; i < value.length; i++)
-                    this.sum[i] += value[i];
+                for (var i = 0; i < delta.length; i++){
+                    this.sum[i] += delta[i];
+                    this.sum[i] = clamp(this.sum[i], this.options.min, this.options.max);
+                }
             }
-            else this.sum += value;
+            else {
+                this.sum += delta;
+                this.sum = clamp(this.sum, this.options.min, this.options.max);
+            }
         }.bind(this));
     }
 
     Accumulator.prototype = Object.create(Stream.prototype);
     Accumulator.prototype.constructor = Accumulator;
+
+    Accumulator.DEFAULT_OPTIONS = {
+        min : -Infinity,
+        max :  Infinity
+    };
+
+    function clamp(value, min, max){
+        return Math.min(Math.max(value, min), max);
+    }
 
     /**
      * Set accumulated value.
@@ -73,8 +90,8 @@ define(function(require, exports, module){
      */
     Accumulator.prototype.set = function(sum, silent){
         this.sum = sum;
-        var self = this;
         if (silent === true) return;
+        var self = this;
         preTickQueue.push(function(){
             self.trigger('start', sum);
             dirtyQueue.push(function(){
