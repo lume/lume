d083b10f082780b242a7a1b98a2f8bee18afa6a7
David Valdman <dmvaldman@gmail.com>
2015-12-18 16:18:56 -0500

    Add inertial support to Scrollview

--- samsara/layouts/Scrollview.js
+++ samsara/layouts/Scrollview.js
@@ -1,6 +1,6 @@
 /* Copyright © 2015 David Valdman */
 
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     var Transform = require('../core/Transform');
     var Transitionable = require('../core/Transitionable');
     var View = require('../core/View');
@@ -18,75 +18,91 @@ define(function(require, exports, module) {
     var TouchInput = require('../inputs/TouchInput');
 
     GenericInput.register({
-        touch : TouchInput,
-        scroll : ScrollInput
+        touch: TouchInput,
+        scroll: ScrollInput
     });
 
     var CONSTANTS = {
-        DIRECTION : {
-            X : 0,
-            Y : 1
+        DIRECTION: {
+            X: 0,
+            Y: 1
         }
     };
 
     var Scrollview = View.extend({
-        defaults : {
-            direction : CONSTANTS.DIRECTION.Y,
-            pageTransition : false
+        defaults: {
+            direction: CONSTANTS.DIRECTION.Y,
+            pageTransition: false
         },
-        initialize : function(options){
+        initialize: function (options) {
             this._currentIndex = 0;
             this._previousIndex = 0;
             this.itemOffset = 0;
             this.items = [];
+
             this.layout = new SequentialLayout({
-                direction : options.direction
+                direction: options.direction
             });
 
             var genericInput = new GenericInput(['touch', 'scroll'], {
-                direction : options.direction
+                direction: options.direction
             });
 
             var position = new Accumulator(0);
-            this.bounce = new Transitionable(0);
 
-            var bounceDifferential = new Differential();
+            this.drag = new Transitionable(0);
+            this.spring = new Transitionable(0);
+
+            var dragDifferential = new Differential();
+            var springDifferential = new Differential();
             var gestureDifferential = genericInput.pluck('delta');
 
-            bounceDifferential.subscribe(this.bounce);
+            dragDifferential.subscribe(this.drag);
+            springDifferential.subscribe(this.spring);
 
             position.subscribe(gestureDifferential);
-            position.subscribe(bounceDifferential);
+            position.subscribe(dragDifferential);
+            position.subscribe(springDifferential);
 
-            if (options.pageTransition){
-                genericInput.on('end', function(){
+            if (options.pageTransition) {
+                genericInput.on('end', function (data) {
                     if (!shouldBounce) return;
-                    this.bounce.set(0);
-                    this.bounce.set(this.itemOffset, options.pageTransition);
+                    this.drag.reset(0);
+                    this.spring.reset(0);
+                    options.pageTransition.velocity = data.velocity + this.drag.getVelocity();
+                    this.drag.set(0, options.pageTransition);
                 }.bind(this));
 
-                genericInput.on('start', function(){
-                    if (this.bounce.isActive())
-                        this.bounce.halt();
+                genericInput.on('start', function () {
+                    this.drag.halt();
+                    this.spring.halt();
                 }.bind(this));
 
-                this.bounce.on('end', function(){
+                this.spring.on('start', function () {
+                    this.drag.velocity = 0;
+                }.bind(this));
+
+                this.spring.on('end', function () {
                     changePage.call(this, this._currentIndex);
                 }.bind(this));
             }
 
-            var overflowStream = ResizeStream.lift(function(contentLength, viewportSize){
+            position.on('end', function(){
+                changePage.call(this, this._currentIndex);
+            }.bind(this));
+
+            var overflowStream = ResizeStream.lift(function (contentLength, viewportSize) {
                 if (!contentLength) return false;
                 return viewportSize[options.direction] - contentLength;
             }, [this.layout, this.size]);
 
 
             var shouldBounce = true;
-            this.offset = Stream.lift(function(top, overflow){
+            this.offset = Stream.lift(function (top, overflow) {
                 shouldBounce = true;
                 if (!overflow) return false;
 
-                if (this.bounce.isActive())
+                if (this.drag.isActive() || this.spring.isActive())
                     return Math.round(top);
 
                 if (top <= overflow) {
@@ -95,7 +111,7 @@ define(function(require, exports, module) {
                     changePage.call(this, this._currentIndex);
                     return overflow;
                 }
-                else if(top >= 0){
+                else if (top >= 0) {
                     shouldBounce = false;
                     position.set(0, true);
                     changePage.call(this, this._currentIndex);
@@ -106,7 +122,7 @@ define(function(require, exports, module) {
             }.bind(this), [position, overflowStream]);
 
             var displacementNode = new LayoutNode({
-                transform : this.offset.map(function(position){
+                transform: this.offset.map(function (position) {
                     return options.direction == CONSTANTS.DIRECTION.Y
                         ? Transform.translateY(position)
                         : Transform.translateX(position);
@@ -114,8 +130,8 @@ define(function(require, exports, module) {
             });
 
             var container = new ContainerSurface({
-                properties : {
-                    overflow : 'hidden'
+                properties: {
+                    overflow: 'hidden'
                 }
             });
 
@@ -124,33 +140,33 @@ define(function(require, exports, module) {
             container.add(displacementNode).add(this.layout);
             this.add(container);
         },
-        goto : function(index, transition, callback){
+        goto: function (index, transition, callback) {
             transition = transition || this.options.transition;
             var position = this.itemOffset;
-            if (index > this._currentIndex){
+            if (index > this._currentIndex) {
                 for (var i = this._currentIndex; i < index; i++)
                     position -= this.items[i].getSize()[this.options.direction];
             }
-            else if (index < this._currentIndex){
+            else if (index < this._currentIndex) {
                 for (var i = this._currentIndex; i > index; i--)
                     position += this.items[i].getSize()[this.options.direction];
             }
             else return;
 
-            this.bounce.set(0);
-            this.bounce.set(Math.ceil(position), transition, callback);
+            this.spring.set(0);
+            this.spring.set(Math.ceil(position), transition, callback);
         },
-        addItems : function(items){
+        addItems: function (items) {
             this.layout.addItems(items);
             this.items = items;
 
             var args = [this.offset];
-            for (var i = 0; i < items.length; i++){
+            for (var i = 0; i < items.length; i++) {
                 args.push(items[i].size);
             }
 
             var accumLength = 0;
-            var itemOffsetStream = Stream.lift(function(){
+            var itemOffsetStream = Stream.lift(function () {
                 if (arguments[1] === undefined) return false;
 
                 var offset = arguments[0];
@@ -164,13 +180,13 @@ define(function(require, exports, module) {
                 var itemOffset = -offset - accumLength;
                 var currentLength = currentSize[direction];
 
-                if (itemOffset >= currentLength){
+                if (itemOffset >= currentLength) {
                     // pass currentNode forwards
                     this._currentIndex++;
                     progress = 0;
                     accumLength += currentLength;
                 }
-                else if (itemOffset < 0){
+                else if (itemOffset < 0) {
                     // pass currentNode backwards
                     if (this._currentIndex == 0) return false;
                     this._currentIndex--;
@@ -187,25 +203,25 @@ define(function(require, exports, module) {
                     : itemOffset - currentLength;
 
                 return {
-                    index : this._currentIndex,
-                    progress : progress
+                    index: this._currentIndex,
+                    progress: progress
                 };
             }.bind(this), args);
 
             this.output.subscribe(itemOffsetStream);
 
-            itemOffsetStream.on('start', function(value){
+            itemOffsetStream.on('start', function (value) {
             }.bind(this));
 
-            itemOffsetStream.on('update', function(value){
+            itemOffsetStream.on('update', function (value) {
             }.bind(this));
 
-            itemOffsetStream.on('end', function(value){
+            itemOffsetStream.on('end', function (value) {
             }.bind(this));
         }
     });
 
-    function changePage(index){
+    function changePage(index) {
         if (index == this._previousIndex) return;
         this.emit('page', index);
         this._previousIndex = index;
