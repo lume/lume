0a15b63ff8449f3bd6107d338cce6843c34feca7
David Valdman <dmvaldman@gmail.com>
2015-12-22 17:25:54 -0500

    Fix testing loop to work in Node and the browser

--- tests/loop.js
+++ tests/loop.js
@@ -1,11 +1,13 @@
 define(function (require, exports, module) {
+    // Creates background render loop for both running in Node and the browser
+
     var Engine = require('samsara/core/Engine');
 
     var loop;
     var cancel = false;
     var running = false;
 
-    if (window) loop = Engine;
+    if (typeof window === 'object') loop = Engine;
     else {
         loop = {
             start : function () {
