576094f4f9e235c1cf3b6cc47bba29b21a8506b3
David Valdman <dmvaldman@gmail.com>
2016-03-06 23:12:00 -0500

    More removal tests

--- test/dom/Removal.js
+++ test/dom/Removal.js
@@ -6,10 +6,12 @@ define(function(require) {
     var View = require('samsara/core/View');
 
     var surfaceClass = '.samsara-surface';
+    var contextClass = '.samsara-context';
+    var containerClass = '.samsara-container';
 
     QUnit.module('DOM Removal');
 
-    QUnit.test('remove surface', function(assert){
+    QUnit.test('Remove Surface', function(assert){
         var context = new Context();
         var mountElement = document.createElement('div');
         var surface = new Surface();
@@ -29,7 +31,7 @@ define(function(require) {
         assert.ok(surfaceEl instanceof Node);
     });
 
-    QUnit.test('remove branch', function(assert) {
+    QUnit.test('Remove Branch', function(assert) {
         var context = new Context();
         var mountElement = document.createElement('div');
         var surface = new Surface();
@@ -59,147 +61,118 @@ define(function(require) {
         assert.ok(surfaces.length === 1);
     });
 
-    //registerSuite({
-    //    name : 'Node Removal',
-    //    removeSurface : function() {
-    //
-    //    },
-    //    removeBranch : function() {
-    //        var context = new Context();
-    //        var mountElement = document.createElement('div');
-    //        var surface = new Surface();
-    //
-    //        // Create branch
-    //        var centerNode = context.add({align : [.5, .5]});
-    //        centerNode.add(surface);
-    //
-    //        // add
-    //        context.add(centerNode);
-    //        context.mount(mountElement);
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isTrue(surfaceEl instanceof Node);
-    //
-    //        // remove
-    //        centerNode.remove();
-    //        surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isFalse(surfaceEl instanceof Node);
-    //
-    //        // add again
-    //        context.add(centerNode);
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isTrue(surfaceEl instanceof Node);
-    //
-    //        // check surfaces have been correctly recycled
-    //        var surfaces = mountElement.querySelectorAll(surfaceClass);
-    //        assert.isTrue(surfaces.length === 1);
-    //    },
-    //    removeContext : function() {
-    //        var context = new Context();
-    //        var mountElement = document.createElement('div');
-    //        var surface = new Surface();
-    //
-    //        // Mount context
-    //        context.add(surface);
-    //        context.mount(mountElement);
-    //
-    //        // remove context
-    //        context.remove();
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isFalse(surfaceEl instanceof Node);
-    //
-    //        // remount context
-    //        context.mount(mountElement);
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isTrue(surfaceEl instanceof Node);
-    //    },
-    //    removeContainer : function() {
-    //        var context = new Context();
-    //        var mountElement = document.createElement('div');
-    //        var surface = new Surface();
-    //
-    //        // create container
-    //        var container = new ContainerSurface();
-    //        container.add(surface);
-    //        context.add(container);
-    //
-    //        // mount context
-    //        context.mount(mountElement);
-    //
-    //        container.remove();
-    //        context.add(container);
-    //    },
-    //    removeContainerChildren : function() {
-    //        var context = new Context();
-    //        var mountElement = document.createElement('div');
-    //
-    //        var surface1 = new Surface({classes : ['test1']});
-    //        var surface2 = new Surface({classes : ['test2']});
-    //
-    //        // create container
-    //        var container = new ContainerSurface();
-    //        container.add(surface1);
-    //        container.add(surface2);
-    //
-    //        // mount context
-    //        context.add(container);
-    //        context.mount(mountElement);
-    //
-    //        // assert both elements are in the DOM
-    //        var surface1El = mountElement.querySelector('.test1');
-    //        var surface2El = mountElement.querySelector('.test2');
-    //        assert.isTrue(surface1El instanceof Node);
-    //        assert.isTrue(surface2El instanceof Node);
-    //
-    //        // remove surface1
-    //        surface1.remove();
-    //        var surface1El = mountElement.querySelector('.test1');
-    //        assert.isFalse(surface1El instanceof Node);
-    //
-    //        // remove surface2
-    //        surface2.remove();
-    //        var surface2El = mountElement.querySelector('.test2');
-    //        assert.isFalse(surface1El instanceof Node);
-    //
-    //        // add surface1
-    //        container.add(surface1);
-    //        var surface1El = mountElement.querySelector('.test1');
-    //        assert.isTrue(surface1El instanceof Node);
-    //
-    //        // remove surface2
-    //        container.add(surface2);
-    //        var surface2El = mountElement.querySelector('.test2');
-    //        assert.isTrue(surface1El instanceof Node);
-    //
-    //        // check total number of surfaces in container is 2
-    //        var surfaces = container._currentTarget.querySelectorAll(surfaceClass);
-    //        assert.isTrue(surfaces.length == 2);
-    //    },
-    //    removeView : function() {
-    //        // create the View
-    //        var MyView = View.extend({
-    //            initialize : function(){
-    //                var surface = new Surface();
-    //                this.add(surface);
-    //            }
-    //        });
-    //
-    //        var myView = new MyView();
-    //
-    //        // mount the context
-    //        var context = new Context();
-    //        var mountElement = document.createElement('div');
-    //        context.add(myView);
-    //        context.mount(mountElement);
-    //
-    //        myView.remove();
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isFalse(surfaceEl instanceof Node);
-    //
-    //        context.add(myView);
-    //        var surfaceEl = mountElement.querySelector(surfaceClass);
-    //        assert.isTrue(surfaceEl instanceof Node);
-    //    },
-    //    branchMove : function() {
-    //    }
-    //});
+    QUnit.test('Remove Context', function(assert) {
+        var context = new Context();
+        var mountElement = document.createElement('div');
+        var surface = new Surface();
+
+        // Mount context
+        context.add(surface);
+        context.mount(mountElement);
+
+        // remove context
+        context.remove();
+        var surfaceEl = mountElement.querySelector(surfaceClass);
+        assert.notOk(surfaceEl instanceof Node);
+
+        // remount context
+        context.mount(mountElement);
+        var surfaceEl = mountElement.querySelector(surfaceClass);
+        assert.ok(surfaceEl instanceof Node);
+    });
+
+    QUnit.test('Remove Container', function(assert) {
+        var context = new Context();
+        var mountElement = document.createElement('div');
+        var surface = new Surface();
+
+        // create container
+        var container = new ContainerSurface();
+        container.add(surface);
+        context.add(container);
+
+        // mount context
+        context.mount(mountElement);
+
+        container.remove();
+        var containerEl = mountElement.querySelector(containerClass);
+        assert.notOk(containerEl instanceof Node);
+
+        context.add(container);;
+        var containerEl = mountElement.querySelector(containerClass);
+        assert.ok(containerEl instanceof Node);
+    });
+
+    QUnit.test('Remove Container Children', function(assert) {
+        var context = new Context();
+        var mountElement = document.createElement('div');
+
+        var surface1 = new Surface({classes : ['test1']});
+        var surface2 = new Surface({classes : ['test2']});
+
+        // create container
+        var container = new ContainerSurface();
+        container.add(surface1);
+        container.add(surface2);
+
+        // mount context
+        context.add(container);
+        context.mount(mountElement);
+
+        // assert both elements are in the DOM
+        var surface1El = mountElement.querySelector('.test1');
+        var surface2El = mountElement.querySelector('.test2');
+        assert.ok(surface1El instanceof Node);
+        assert.ok(surface2El instanceof Node);
+
+        // remove surface1
+        surface1.remove();
+        var surface1El = mountElement.querySelector('.test1');
+        assert.notOk(surface1El instanceof Node);
+
+        // remove surface2
+        surface2.remove();
+        var surface2El = mountElement.querySelector('.test2');
+        assert.notOk(surface1El instanceof Node);
+
+        // add surface1
+        container.add(surface1);
+        var surface1El = mountElement.querySelector('.test1');
+        assert.ok(surface1El instanceof Node);
+
+        // remove surface2
+        container.add(surface2);
+        var surface2El = mountElement.querySelector('.test2');
+        assert.ok(surface1El instanceof Node);
+
+        // check total number of surfaces in container is 2
+        var surfaces = container._currentTarget.querySelectorAll(surfaceClass);
+        assert.ok(surfaces.length == 2);
+    });
+
+    QUnit.test('Remove View', function(assert) {
+        // create the View
+        var MyView = View.extend({
+            initialize : function(){
+                var surface = new Surface();
+                this.add(surface);
+            }
+        });
+
+        var myView = new MyView();
+
+        // mount the context
+        var context = new Context();
+        var mountElement = document.createElement('div');
+        context.add(myView);
+        context.mount(mountElement);
+
+        myView.remove();
+        var surfaceEl = mountElement.querySelector(surfaceClass);
+        assert.notOk(surfaceEl instanceof Node);
+
+        context.add(myView);
+        var surfaceEl = mountElement.querySelector(surfaceClass);
+        assert.ok(surfaceEl instanceof Node);
+    });
 });
