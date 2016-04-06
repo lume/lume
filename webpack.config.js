46984dcd92be361cff06c498fcac21cbf62318b2
Joseph Orbegoso Pea <joe@trusktr.io>
2016-04-06 14:05:31 -0700

    Restore missing webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,34 @@
-What happened to webpack config? Get from macbook backup.
+module.exports = {
+    entry: [
+        // Using this instead of transform-runtime,
+        // see babel config below.
+        'babel-polyfill',
+    ],
+    resolve: {
+        extensions: [ "", ".js", ".jsx", ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    babelrc: false,
+                    presets: [
+                        'es2015',
+                        'stage-0',
+                    ],
+                    plugins: [
+                        // broken in Webpack, doesn't work. Instead we load
+                        // babel-polyfill as an entry, above. See
+                        // https://discuss.babeljs.io/t/regeneratorruntime-is-not-defined-in-webpack-build/202
+                        //'transform-runtime'
+                    ],
+                },
+            },
+        ],
+    },
+    devtool: "source-map",
+}
