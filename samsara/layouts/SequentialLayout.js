12754c12e58c6b6868bf735fa1e002594396b3e5
David Valdman <dmvaldman@gmail.com>
2016-05-01 00:43:29 -0400

    SequentialLayout uses SizeArray

--- samsara/layouts/SequentialLayout.js
+++ samsara/layouts/SequentialLayout.js
@@ -1,13 +1,12 @@
 /* Copyright © 2015-2016 David Valdman */
-
 define(function(require, exports, module) {
     var Transform = require('../core/Transform');
     var View = require('../core/View');
-    var Stream = require('../streams/Stream');
+    var SizeArray = require('../streams/SizeArray');
 
     var CONSTANTS = {
         DIRECTION : {
-            X : 0,
+            X : 0, 
             Y : 1
         }
     };
@@ -28,55 +27,29 @@ define(function(require, exports, module) {
         defaults : {
             direction : CONSTANTS.DIRECTION.X,
             spacing : 0
-        },
-        initialize : function initialize(){},
-        /**
-         * Add content as an array of Views or Surfaces.
-         *
-         * @method addItems
-         * @param items {Array}  An array of Views or Surfaces
-         */
-        addItems : function addItems(items){
-            var sizes = [];
-            for (var i = 0; i < items.length; i++)
-                sizes.push(items[i].size);
-
-            var stream = Stream.lift(function(){
-                var sizes = arguments;
-                var direction = this.options.direction;
-                var transforms = [];
-
-                var length = 0;
-                for (var i = 0; i < sizes.length; i++){
-                    var size = sizes[i];
-
-                    var transform = direction === CONSTANTS.DIRECTION.X
-                        ? Transform.translateX(length)
-                        : Transform.translateY(length);
-
-                    transforms.push(transform);
-
-                    length += size[direction] + this.options.spacing;
-                }
-
-                return {
-                    transforms : transforms,
-                    length: length
-                };
-            }.bind(this), sizes);
-
-            var transforms = stream.pluck('transforms');
-            var length = stream.pluck('length');
-
-            this.output.subscribe(length);
-
-            for (var i = 0; i < items.length; i++){
-                var node = items[i];
-                var transform = transforms.pluck(i);
-                this.add({transform : transform}).add(node);
-            }
+        }, 
+        initialize : function initialize(options) {
+            this.items = [];
+            
+            this.stream = new SizeArray(function(prev, size) {
+                return prev + size[options.direction] + options.spacing;
+            });
+            
+            this.transformMap = function(length) {
+                return (options.direction === CONSTANTS.DIRECTION.X) ? Transform.translateX(length) : Transform.translateY(length);
+            };
+        }, 
+        addItem : function(item) {
+            var transform = this.stream.addStream(item.size).map(this.transformMap);
+            this.add({transform : transform}).add(item);
+        }, 
+        removeItem : function(item) {
+            this.stream.removeStream(item.size);
+            item.remove();
+        }, 
+        insertItem : function(index) {
         }
     }, CONSTANTS);
-
+    
     module.exports = SequentialLayout;
-});
+}); 
