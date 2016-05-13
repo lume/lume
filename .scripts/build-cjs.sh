a22c85fb67f4b917373dfe3eae65959fdc4afc8b
Joseph Orbegoso Pea <joe@trusktr.io>
2016-05-13 13:34:21 -0700

    Add watch script for automatic building when files change. Makes `npm link` workflow easier (as per changes to trusktr/npm-package-seleton).

--- .scripts/build-cjs.sh
+++ .scripts/build-cjs.sh
@@ -1,1 +1,1 @@
-babel src --source-maps --out-dir . --plugins=transform-es2015-modules-commonjs
+babel src --source-maps --out-dir . --plugins=transform-es2015-modules-commonjs $1
--- .scripts/build-global.sh
+++ .scripts/build-global.sh
@@ -7,4 +7,4 @@
 # browser).
 package_name='infamous'
 
-webpack --progress --colors --output-library-target umd src/index.js global.js --output-library $package_name
+webpack --progress --colors --output-library-target umd src/index.js global.js --output-library $package_name $1
