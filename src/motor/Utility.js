1839ee50666074ceb662639b175a84fe4adee98d
Joseph Orbegoso Pea <joe@trusktr.io>
2016-05-13 18:30:49 -0700

    Removed startLoop, stopLoop, and startTween, which will be replaced by Motor.addRenderTask.

--- src/motor/Utility.js
+++ src/motor/Utility.js
@@ -1,55 +1,6 @@
 import TWEEN from 'tween.js'
 import windowLoaded from 'awaitbox/dom/windowLoaded'
 
-let rAF = null // reference to the rAF loop used for animating everything.
-
-function startTween(tween) {
-    tween.start()
-    startLoop()
-}
-
-// Render the scene repeatedly. In the future we can render only the needed
-// parts.
-function startLoop() {
-    // start an rAF loop if there isn't one.
-    if (!rAF) {
-        rAF = window.requestAnimationFrame(function tick() {
-
-            // (errors could happen in the TWEEN.update() call below, so we
-            // catch them and rethrow them, for now.)
-            try {
-
-                // If we have a reason to request a new frame, continue.
-                if (TWEEN.getAll().length) { // if we have tweens left to animate.
-                    TWEEN.update() // update all tweens
-                    // ^ TODO: pass in a custom timestamp here. We'll need to
-                    // do this in order to synchronize all the parts (DOM
-                    // renderer, WebGL renderer, etc).
-
-                    rAF = window.requestAnimationFrame(tick)
-                }
-
-                // Otherwise don't request the next frame, and nullify the rAF
-                // reference.
-                else {
-                    rAF = null
-                }
-            }
-            catch (error) {
-                throw error
-            }
-        })
-    }
-}
-
-// TODO: How do we pause tweens?
-function stopLoop() {
-    if (rAF) {
-        window.cancelAnimationFrame(rAF)
-        rAF = null
-    }
-}
-
 function epsilon(value) {
     return Math.abs(value) < 0.000001 ? 0 : value;
 }
@@ -102,9 +53,6 @@ function makeLowercaseSetterAliases(object) {
 }
 
 export {
-  startTween,
-  startLoop,
-  stopLoop,
   epsilon,
   applyCSSLabel,
   getBodySize,
