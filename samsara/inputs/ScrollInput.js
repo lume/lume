7018623c92e067ba792b59a0e6fda4ab791ed0ce
David Valdman <dmvaldman@gmail.com>
2016-06-26 17:43:53 -0400

    Update licenses

--- samsara/inputs/ScrollInput.js
+++ samsara/inputs/ScrollInput.js
@@ -1,12 +1,4 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * @license MPL 2.0
- * @copyright Famous Industries, Inc. 2014
- */
-
-/* Modified work copyright © 2015-2016 David Valdman */
+/* Copyright © 2015-2016 David Valdman */
 
 define(function(require, exports, module) {
     var EventHandler = require('../events/EventHandler');
