0deda57ac46202dd39b716f207fb4e9f04a49b69
Joseph Orbegoso Pea <joe@trusktr.io>
2016-07-06 14:47:59 -0700

    Set "browser" field in package.json to index.js, which is the file browsers should using when package.json is relevant (when using modules in the browser via Webpack, Browserify, JSPM, etc). The global.js file is relevant when package.json isn't relevant, and the user is relying on globals.

--- src/motor-html/index.js
+++ src/motor-html/index.js
@@ -1,3 +1,4 @@
 
 export MotorHTMLNode from './node'
 export MotorHTMLScene from './scene'
+export MotorHTMLPushPaneLayout from './push-pane-layout'
--- src/motor/index.js
+++ src/motor/index.js
@@ -2,5 +2,6 @@
 export Curves from './Curves'
 export Node from './Node'
 export Scene from './Scene'
+export PushPaneLayout from './PushPaneLayout'
 
 export * from './Utility'
