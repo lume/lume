f63a3d8dfe62bd86a8616a1282f2ab40a9a221cd
David Valdman <dmvaldman@gmail.com>
2016-07-06 23:21:07 -0400

    Update Logo example surface sizing

--- examples/Logo/js/main.js
+++ examples/Logo/js/main.js
@@ -4,8 +4,10 @@ define(function (require, exports, module) {
 
     // Create the logo
     var logo = new Logo({
-        proportions: [1 / 3, false], // The logo's width will be 1/2 the screen
-        aspectRatio: 1,              // Forces logo's height = width
+        size: function(parentSize){
+            var length = 0.5 * Math.min(parentSize[0], parentSize[1]);
+            return [length, length];
+        },
         origin: [.5, .5],            // Origin point of the logo for centering
         sides: 6                     // Number of wedges
     });
