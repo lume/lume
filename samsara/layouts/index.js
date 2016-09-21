116f15e03a6d7f99f585c9d5ded35af3ccbb5530
David Valdman <dmvaldman@gmail.com>
2016-09-20 23:04:28 -0400

    Update index.js for layouts

--- samsara/layouts/index.js
+++ samsara/layouts/index.js
@@ -1,9 +1,10 @@
 define(function(require, exports, module) {
     module.exports = {
         DrawerLayout: require('./DrawerLayout'),
-        FlexibleLayout: require('./FlexibleLayout'),
+        FlexibleLayout: require('./FlexLayout'),
         GridLayout: require('./GridLayout'),
         SequentialLayout: require('./SequentialLayout'),
+        HeaderFooterLayout: require('./HeaderFooterLayout'),
         Scrollview: require('./Scrollview')
     };
 });
