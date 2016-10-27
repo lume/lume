9b53e83eadf126444e1a30f3e6b43691476ff1be
David Valdman <dmvaldman@gmail.com>
2016-10-26 23:25:00 -0400

    Update Sphere example with gradient surface

--- examples/Sphere/js/main.js
+++ examples/Sphere/js/main.js
@@ -1,10 +1,13 @@
 define(function(require, exports, module) {
     var Context = require('samsara/dom/Context');
-    var Viewer = require('samsara/camera/Viewer');
+    var ArcballCamera = require('samsara/camera/TrackballCamera');
     var Sphere = require('./app/Sphere');
+    var Surface = require('samsara/dom/Surface');
+    var Transform = require('samsara/core/Transform');
+    var Quaternion = require('samsara/camera/Quaternion');
 
-    // Radius of sphere
-    var radius = Math.min(window.innerWidth/2, 300);
+    // Radius of sphere (fixed)
+    var radius = Math.min(window.innerWidth/2, window.innerHeight/2, 300);
 
     // Transition definitions
     var lookAtTransition = {    // spring transition to look at a surface when clicked
@@ -33,31 +36,49 @@ define(function(require, exports, module) {
 
     // Create "arc-ball" camera starting at [0, 0, -radius]
     var cameraStartPosition = [0, 0, -radius];
-    var viewer = new Viewer({
+    var camera = new ArcballCamera({
         radius : radius,
         position: cameraStartPosition
     });
 
     // Viewer gets mouse/touch events from context
-    viewer.subscribe(context);
+    camera.subscribe(context);
 
     // Rotate the sphere to look at a surface when it's clicked
     sphere.on('lookAt', function(transform){
-        viewer.setPosition([0,0,0], lookAtTransition);
-        viewer.lookAtTransform(transform, lookAtTransition);
+        camera.setPosition([0,0,0], lookAtTransition);
+        camera.lookAt(transform, lookAtTransition);
     });
 
     // Zoom out when the context is clicked
     context.on('click', function(){
-        viewer.setPosition(cameraStartPosition, lookAtTransition);
+        camera.setPosition(cameraStartPosition, lookAtTransition);
     });
 
-    // Create the render tree
+    // Create the render tree for the sphere
     var cameraNode = context
-        .add(viewer)
+        .add(camera)
         .add({align : [.5,.5]})
         .add(sphere);
 
+    // Add a surface in the middle of the sphere that is a
+    // transparent gradient that darkens the back hemisphere
+    var gradientSurface = new Surface({
+        size : [2*radius, 2*radius], // size of sphere
+        origin : [.5,.5],
+        classes : ['gradient']
+    });
+
+    // Transform the gradient surface to follow the sphere
+    var gradientSurfaceTransform = camera.orientation.map(function(rotation){
+        return Quaternion.toTransform(rotation);
+    });
+
+    // Create the render tree for the gradient surface
+    cameraNode
+        .add({transform : gradientSurfaceTransform})
+        .add(gradientSurface);
+
     // Mount to the DOM
-    context.mount(document.body);
+    context.mount(document.querySelector('#sphere'));
 });
