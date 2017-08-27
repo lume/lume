cd8a1d1ed384ce430c7f3361c213ed1683acf0fb
Joe Pea <joe@trusktr.io>
2017-08-27 02:46:50 -0700

    Noted a comment on a Rollup bundle issue that I need to fix before I can enable the Rollup global build.

--- .scripts/build-global.sh
+++ .scripts/build-global.sh
@@ -8,4 +8,19 @@
 package_name='infamous'
 
 webpack --display-optimization-bailout --progress --colors --output-library-target umd src/index.js global.js --output-library $package_name $1
+
+# TODO FIXME: When we enable Rollup instead of Webpack we get this error:
+#
+# 'default' is not exported by 'node_modules/core-js/library/modules/es6.object.to-string.js'
+#
+# which seems to hint that some module is compiled by Babel (because it imports
+# core-js stuff). In commit 1e241eb3 I mentioned that the problem might be with
+# DOMMatrix.
+#
+# Using the namedExports option (for Buble) with "default" as one of the
+# possible named exports (in rollup.config.js) doesn't work. See the
+# namedExports definition in rollup.config.js.
+#
+# Unfortunately, I didn't write how to reproduce. I think maybe despite the
+# error the bundle might be made and running it will show the runtime error...?
 #rollup -c
--- rollup.config.js
+++ rollup.config.js
@@ -23,6 +23,10 @@ export default {
                 modules: false,
                 dangerousForOf: true,
             },
+
+            namedExports: {
+                'core-js/library/modules/es6.object.to-string.js': 'default',
+            },
         }),
 
         babili({
