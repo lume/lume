d633c40bf99a6b1dc5cd46c42274ff934413857c
Joe Pea <joe@trusktr.io>
2017-10-30 07:58:50 -0700

    Add color attribute handling for materials. duplicate code needs to be consolidated, but more generally we will come up with a way to register geometry and material behaviors, and this will consolidate duplicate code

--- src/html/behaviors/BasicMaterialBehavior.js
+++ src/html/behaviors/BasicMaterialBehavior.js
@@ -1,9 +1,13 @@
 import './HasAttribute'
-import { MeshBasicMaterial } from 'three'
+import { MeshBasicMaterial, Color } from 'three'
 import BaseMeshBehavior from './BaseMeshBehavior'
 
 export default
 class BasicMaterialBehavior extends BaseMeshBehavior {
+    static get observedAttributes() {
+        return [ 'color' ]
+    }
+
     async connectedCallback( element ) {
         if ( ! this.checkedElementIsMesh ) await this.checkElementIsMesh(element)
         if ( ! this.elementIsMesh ) return
@@ -20,9 +24,29 @@ class BasicMaterialBehavior extends BaseMeshBehavior {
         element._needsToBeRendered()
     }
 
-    // TODO
-    attributeChangedCallback() {
-        // set color, etc.
+    // TODO: duplicate attribute code between material behaviors, consolidate
+    async attributeChangedCallback(element, attr, oldVal, newVal) {
+        if ( ! this.checkedElementIsMesh ) await this.checkElementIsMesh(element)
+        if ( ! this.elementIsMesh ) return
+
+        if ( attr == 'color' ) {
+
+            // TODO: generic type system for attributes. It will eliminate
+            // duplication here (see duplicated code in PointLight class).
+
+            // if a triplet space-separated of RGB numbers
+            if ( newVal.match( /^\s*\d+\s+\d+\s+\d+\s*$/ ) ) {
+                newVal = newVal.trim().split( /\s+/ ).map( n => parseFloat(n)/255 )
+                element.threeObject3d.material.color = new Color( ...newVal )
+            }
+            // otherwise a CSS-style color string
+            else {
+                element.threeObject3d.material.color = new Color( newVal )
+            }
+
+            element._needsToBeRendered()
+
+        }
     }
 }
 
--- src/html/behaviors/PhongMaterialBehavior.js
+++ src/html/behaviors/PhongMaterialBehavior.js
@@ -1,26 +1,51 @@
 import './HasAttribute'
-import { MeshPhongMaterial, MeshBasicMaterial } from 'three'
+import { MeshPhongMaterial, Color } from 'three'
 import BaseMeshBehavior from './BaseMeshBehavior'
 
 export default
 class PhongMaterialBehavior extends BaseMeshBehavior {
+    static get observedAttributes() {
+        return [ 'color' ]
+    }
+
     async connectedCallback( element ) {
-        if ( ! await this.checkElementIsMesh(element) ) return
+        if ( ! this.checkedElementIsMesh ) await this.checkElementIsMesh(element)
+        if ( ! this.elementIsMesh ) return
 
         this.setMeshComponent( element, 'material', new MeshPhongMaterial({ color: 0x00ff00 }) )
         element._needsToBeRendered()
     }
 
     async disconnectedCallback( element ) {
-        if ( ! await this.checkElementIsMesh(element) ) return
+        if ( ! this.checkedElementIsMesh ) await this.checkElementIsMesh(element)
+        if ( ! this.elementIsMesh ) return
 
         this.setDefaultComponent( element, 'material' )
         element._needsToBeRendered()
     }
 
-    // TODO
-    attributeChangedCallback() {
-        // set color, etc.
+    async attributeChangedCallback(element, attr, oldVal, newVal) {
+        if ( ! this.checkedElementIsMesh ) await this.checkElementIsMesh(element)
+        if ( ! this.elementIsMesh ) return
+
+        if ( attr == 'color' ) {
+
+            // TODO: generic type system for attributes. It will eliminate
+            // duplication here (see duplicated code in PointLight class).
+
+            // if a triplet space-separated of RGB numbers
+            if ( newVal.match( /^\s*\d+\s+\d+\s+\d+\s*$/ ) ) {
+                newVal = newVal.trim().split( /\s+/ ).map( n => parseFloat(n)/255 )
+                element.threeObject3d.material.color = new Color( ...newVal )
+            }
+            // otherwise a CSS-style color string
+            else {
+                element.threeObject3d.material.color = new Color( newVal )
+            }
+
+            element._needsToBeRendered()
+
+        }
     }
 }
 
