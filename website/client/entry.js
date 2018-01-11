50440ab45d540ffbf9275f041b49251b3c72a8e7
Joe Pea <joe@trusktr.io>
2018-01-10 16:30:33 -0800

    separate the spiral graphic into it's own React component for performance (less to re-render in the component render() method)

--- website/client/entry.js
+++ website/client/entry.js
@@ -29,45 +29,73 @@ const demos = _.shuffle([
     '//trusktr.io/rainbowTriangles',
 ])
 
+// copied from https://codepen.io/trusktr/live/JMMXPB
+class MorphingColorSpiral extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            rotation: 0,
+        }
+
+        const nodes = Array(400).fill(0)
+
+        this.staticInnerScene = (
+            <motor-scene id="spiral-inner-scene">
+                {nodes.map((n, i) => (
+                    <motor-node
+                        key={i}
+                        absoluteSize="0 0 0"
+                        align="0.5 0.5"
+                        rotation={[0, 0, i*10]}
+                    >
+                        <motor-node
+                            absoluteSize={[50-i%50, 50-i%50, 0]}
+                            mountPoint="0.5 0.5"
+                            position={[0, i*2, 0]}
+                            style={{
+                                background: `hsl( ${ (i*2)%360 }, 90%, 78%)`,
+                                borderRadius: `${ i%50 }px`,
+                            }}
+                        >
+                        </motor-node>
+                    </motor-node>
+                ))}
+            </motor-scene>
+        )
+    }
+
+    render() {
+        return (
+            <motor-scene id="spiral-outer-scene" style={{background:'#333'}}>
+                <motor-node
+                    absoluteSize="1630 1630"
+                    align="0.5 0.5"
+                    mountPoint="0.5 0.5"
+                    rotation={[0, 0, this.state.rotation]}
+                >
+
+                    {this.staticInnerScene}
+
+                </motor-node>
+            </motor-scene>
+        )
+    }
+
+    componentDidMount() {
+        Motor.addRenderTask(() => {
+            this.setState({
+                rotation: this.state.rotation - 9.8
+            })
+        })
+    }
+}
+
 main()
 async function main() {
     const footerHeight = 44.5
 
     class App extends React.Component {
-        constructor(props) {
-            super(props)
-
-            this.state = {
-                rotation: 0,
-            }
-
-            const nodes = Array(400).fill(0)
-
-            this.staticInnerScene = (
-                <motor-scene id="spiral-inner-scene">
-                    {nodes.map((n, i) => (
-                        <motor-node
-                            key={i}
-                            absoluteSize="0 0 0"
-                            align="0.5 0.5"
-                            rotation={[0, 0, i*10]}
-                        >
-                            <motor-node
-                                absoluteSize={[50-i%50, 50-i%50, 0]}
-                                mountPoint="0.5 0.5"
-                                position={[0, i*2, 0]}
-                                style={{
-                                    background: `hsl( ${ (i*2)%360 }, 90%, 78%)`,
-                                    borderRadius: `${ i%50 }px`,
-                                }}
-                            >
-                            </motor-node>
-                        </motor-node>
-                    ))}
-                </motor-scene>
-            )
-
-        }
         render() {
             return (
                 <motor-scene ref="scene">
@@ -82,19 +110,7 @@ async function main() {
                             proportionalSize="1 1"
                         >
 
-                            <motor-scene id="spiral-outer-scene" style={{background:'#333',}}>
-                                <motor-node
-                                    absoluteSize="1630 1630"
-                                    align="0.5 0.5"
-                                    mountPoint="0.5 0.5"
-                                    rotation={[0, 0, this.state.rotation]}
-                                >
-
-                                    {this.staticInnerScene}
-
-                                </motor-node>
-                            </motor-scene>
-                            {/*<iframe src="https://codepen.io/trusktr/live/JMMXPB" className="background-iframe"></iframe>*/}
+                            <MorphingColorSpiral />
 
                         </motor-node>
 
@@ -102,7 +118,7 @@ async function main() {
                             class="gradient-background"
                             sizeMode="proportional proportional"
                             proportionalSize="1 1"
-                            opacity="0.95"
+                            opacity="0.85"
                         >
                         </motor-node>
 
@@ -180,12 +196,6 @@ async function main() {
                 this.updateSize()
                 this.refs.scene.imperativeCounterpart.on('sizechange', () => this.updateSize())
             }, 0)
-
-            Motor.addRenderTask(() => {
-                this.setState({
-                    rotation: this.state.rotation - 9.8
-                })
-            })
         }
     }
 
