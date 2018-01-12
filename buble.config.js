3f91267f28860c2c9b390ed292ffa4463d02ae00
Joe Pea <joe@trusktr.io>
2018-01-11 21:15:17 -0800

    re-enabled the Custom Elements v1 polyfill by WebReflection (document-register-element)

--- buble.config.js
+++ buble.config.js
@@ -4,5 +4,11 @@ module.exports = {
     transforms: {
         modules: false,
         dangerousForOf: true,
+
+        // keep this disabled to skip processing template strings and transpile
+        // with another tool. Otherwise if we transpile them with Buble and we
+        // need template tag functions, they won't work, tag functions are not
+        // supported by Buble.
+        templateString: false,
     },
 }
--- src/html/polyfillCustomElements.js
+++ src/html/polyfillCustomElements.js
@@ -1,5 +1,5 @@
 import installCE from 'document-register-element/pony'
-//installCE(window, {
-    //type: 'auto', // default: 'auto'
-    //noBuiltIn: false, // default: false
-//})
+installCE(window, {
+    type: 'auto', // default: 'auto'
+    noBuiltIn: false, // default: false
+})
