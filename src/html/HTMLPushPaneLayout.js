0a2796e5563beb8743f82f1882aa2662f11ec049
Joe Pea <joe@trusktr.io>
2018-04-22 00:33:14 -0700

    convert HTMLPushPaneLayout to lowclass

--- src/html/HTMLPushPaneLayout.js
+++ src/html/HTMLPushPaneLayout.js
@@ -1,15 +1,16 @@
 import HTMLNode from './HTMLNode'
 import PushPaneLayout from '../components/PushPaneLayout'
 
-class HTMLPushPaneLayout extends HTMLNode {
-    static define(name) {
-        customElements.define(name || 'i-push-pane-layout', HTMLPushPaneLayout)
-    }
+export default
+HTMLNode.subclass( 'HTMLPushPaneLayout', {
+    static: {
+        define(name) {
+            customElements.define(name || 'i-push-pane-layout', HTMLPushPaneLayout)
+        }
+    },
 
     // @override
     _makeImperativeCounterpart() {
         return new PushPaneLayout({}, this)
-    }
-}
-
-export {HTMLPushPaneLayout as default}
+    },
+})
