06a4dd8a90bb04c861cff9468701013ab4715fb0
Joe Pea <joe@trusktr.io>
2018-04-23 15:17:37 -0700

    convert Plane to lowclass

--- src/core/Plane.js
+++ src/core/Plane.js
@@ -1,17 +1,20 @@
+import Class from 'lowclass'
+import {native} from 'lowclass/native'
 import Mesh from './Mesh'
 
 export default
-class Plane extends Mesh {
-    static get defaultElementName() { return 'i-plane' }
+Class('Plane').extends( native(Mesh), {
 
-    static get defaultBehaviors() {
-        return {
+    static: {
+        defaultElementName: 'i-plane',
+
+        defaultBehaviors: {
             'plane-geometry': initialBehaviors => {
                 return !initialBehaviors.some( b => b.endsWith( '-geometry' ) )
             },
             'phong-material': initialBehaviors => {
                 return !initialBehaviors.some( b => b.endsWith( '-material' ) )
             },
-        }
-    }
-}
+        },
+    },
+})
