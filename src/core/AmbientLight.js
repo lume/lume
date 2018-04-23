fbc30b4fc19928e348086103e6e39864c75713cc
Joe Pea <joe@trusktr.io>
2018-04-22 20:56:55 -0700

    convert AmbientLight to lowclass

--- src/core/AmbientLight.js
+++ src/core/AmbientLight.js
@@ -1,18 +1,21 @@
+import Class from 'lowclass'
+import {native} from 'lowclass/native'
 import LightBase from './LightBase'
 import { AmbientLight as ThreeAmbientLight } from 'three'
 
 export default
-class AmbientLight extends LightBase {
-    static get defaultElementName() { return 'i-ambient-light' }
-    static get _Class() { return AmbientLight }
+Class('AmbientLight').extends( native(LightBase), {
+    static: {
+        defaultElementName: 'i-ambient-light',
+    },
 
     construct(options = {}) {
         super.construct(options)
-    }
+    },
 
     makeThreeObject3d() {
         const light = new ThreeAmbientLight
         light.intensity = 1 // default
         return light
-    }
-}
+    },
+})
