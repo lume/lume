69f0018e6bfa3994b68a3090ce227e9bc6072c81
Joe Pea <joe@trusktr.io>
2018-04-29 15:26:02 -0700

    update builder-js-package and enforce 2.5% code coverage (for now, while we work to increase it)

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    coverageThreshold: {
+        "global": {
+            "branches": 2.5,
+            "functions": 2.5,
+            "lines": 2.5,
+            "statements": 2.5,
+        },
+    },
+}
