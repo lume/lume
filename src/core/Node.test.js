f720372df5143897c350390c5fc53f97f9059e37
Joe Pea <joe@trusktr.io>
2018-12-06 23:51:04 -0800

    update the Node tests to use 'new' now that the previous commit fixed native extension

--- src/core/Node.test.js
+++ src/core/Node.test.js
@@ -9,23 +9,18 @@ const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 describe('Node', () => {
 
-    let scene = document.createElement('i-scene')
-    // let scene = new Scene
+    let scene = new Scene
 	const body = document.body
 
     afterEach(() => {
 		scene.unmount()
 		body.innerHTML = ''
-		scene = document.createElement('i-scene')
-		// scene = new Scene
+		scene = new Scene
 		scene.mount(body)
 	})
 
     it('default values', async () => {
-        // useDefaultNames()
-
-		const n = document.createElement('i-node')
-		// const n = new Node // FIXME why doesn't this work?
+		const n = new Node
 
 		expect(n.position.x).toEqual(0)
 		expect(n.position.y).toEqual(0)
@@ -58,15 +53,24 @@ describe('Node', () => {
 		expect(n.sizeMode.z).toEqual('literal')
     })
 
-    it('element is an instance of Node', async () => {
-        // useDefaultNames()
-
-		const n = document.createElement('i-node')
-        // const n = new Node
+    it('element is an instance of Node, created with `new`', async () => {
+        const n = new Node
 
 		scene.add(n)
 
-		// expect(n instanceof Node).toBe(true) // FIXME why does this fail?
+		expect(n instanceof Node).toBe(true)
+		expect(n.constructor.name).toBe('Node')
+        expect(n.threeObject3d).not.toBeUndefined()
+        expect(n.scene).not.toBeUndefined()
+        expect(n.scene.constructor.name).toBe('Scene')
+    })
+
+    it('element is an instance of Node, created with `document.createElement`', async () => {
+		const n = document.createElement('i-node')
+
+		scene.add(n)
+
+		expect(n instanceof Node).toBe(true)
 		expect(n.constructor.name).toBe('Node')
         expect(n.threeObject3d).not.toBeUndefined()
         expect(n.scene).not.toBeUndefined()
