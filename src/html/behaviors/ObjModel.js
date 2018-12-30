0ffcaa566d1d3bfce5caec8618eb463f1fada80f
Joe Pea <trusktr@gmail.com>
2018-12-29 23:44:39 -0800

    initial obj-model behavior for loading an OBJ/MTL resource via HTML
    Usage:
    
    <i-node
      has="obj-model"
      obj="path/to/file.obj"
      mtl="path/to/file.mtl">
    </i-node>

---
+++ src/html/behaviors/ObjModel.js
@@ -0,0 +1,99 @@
+import 'element-behaviors'
+import Class from 'lowclass'
+import '../../../lib/three/global'
+import 'three/examples/js/loaders/OBJLoader'
+import 'three/examples/js/loaders/MTLLoader'
+import Behavior from './Behavior'
+
+const ObjModel = Class('ObjModel').extends(Behavior, ({Super}) => ({
+    static: {
+        props: {
+            obj: String, // path to obj file
+            mtl: String, // path to mtl file
+        },
+    },
+
+    updated(oldProps, newProps, modifiedProps) {
+        if (modifiedProps.obj || modifiedProps.mtl) {
+            if (!this.obj) return
+            this.cleanup()
+            this.load()
+        }
+    },
+
+    connectedCallback() {
+        Super(this).connectedCallback()
+        this.model = null
+        this.objLoader = new THREE.OBJLoader()
+        this.mtlLoader = new THREE.MTLLoader(this.objLoader.manager)
+        // Allow cross-origin images to be loaded.
+        this.mtlLoader.crossOrigin = ''
+
+        this.objLoader.manager.onLoad = () => {
+            this.element._needsToBeRendered()
+        }
+    },
+
+    disconnectedCallback() {
+        Super(this).disconnectedCallback()
+        this.cleanup()
+    },
+
+    cleanup() {
+        if (!this.model) return
+        this.element.threeObject3d.remove(this.model) // TODO dispose it
+    },
+
+    load() {
+        const { obj, mtl, mtlLoader, objLoader } = this
+
+        if (mtl) {
+            mtlLoader.setTexturePath(mtl.substr(0, mtl.lastIndexOf('/') + 1))
+            mtlLoader.load(mtl, materials => {
+                materials.preload()
+                objLoader.setMaterials(materials)
+                objLoader.load(obj, model => this.setModel(model))
+            })
+        }
+        else {
+            objLoader.load(obj, model => {
+                // if no material, make a default one with random color
+                const color =  0xffffff/3 * Math.random() + 0xffffff/3
+                setRandomColorPhongMaterial(model)
+
+                this.setModel(model)
+            })
+        }
+    },
+
+    setModel(model) {
+        this.element.threeObject3d.add(this.model = model)
+        this.element.emit('model-loaded', {format: 'obj', model: model})
+        this.element._needsToBeRendered()
+    },
+
+}))
+
+function setColorPhongMaterial(obj, color, dispose, traverse = true) {
+    const material = new THREE.MeshPhongMaterial
+    material.color = new THREE.Color( color )
+
+    if (traverse) obj.traverse(node => applyMaterial(node, material))
+    else applyMaterial(obj, material)
+}
+
+function applyMaterial(obj, material, dispose = true) {
+    if ('material' in obj) {
+        if (obj.material && dispose) obj.material.dispose()
+        obj.material = material
+    }
+}
+
+function setRandomColorPhongMaterial(obj, dispose, traverse) {
+    const randomColor = 0xffffff/3 * Math.random() + 0xffffff/3
+    setColorPhongMaterial( obj, randomColor, dispose, traverse )
+}
+
+elementBehaviors.define('obj-model', ObjModel)
+
+export default ObjModel
