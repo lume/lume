77ab016746f57bb964e528865c598e007a92e08f
Joe Pea <joe@trusktr.io>
2019-03-11 00:15:07 -0700

    fix inconsistent spacing

--- src/html/utils.js
+++ src/html/utils.js
@@ -1,21 +1,21 @@
-export function dashCase(str){
-  return typeof str === 'string'
-    ? str.split(/([_A-Z])/).reduce((one, two, idx) => {
-        const dash = !one || idx % 2 === 0 ? '' : '-'
-        two = two === '_' ? '' : two
-        return `${one}${dash}${two.toLowerCase()}`
-      })
-    : str
+export function dashCase(str) {
+    return typeof str === 'string'
+        ? str.split(/([_A-Z])/).reduce((one, two, idx) => {
+              const dash = !one || idx % 2 === 0 ? '' : '-'
+              two = two === '_' ? '' : two
+              return `${one}${dash}${two.toLowerCase()}`
+          })
+        : str
 }
 
-export const empty = (val) => val == null
+export const empty = val => val == null
 
 export function keys(obj) {
-  obj = obj || {}
-  const names = Object.getOwnPropertyNames(obj)
-  return Object.getOwnPropertySymbols
-    ? names.concat(Object.getOwnPropertySymbols(obj))
-    : names
+    obj = obj || {}
+    const names = Object.getOwnPropertyNames(obj)
+    return Object.getOwnPropertySymbols
+        ? names.concat(Object.getOwnPropertySymbols(obj))
+        : names
 }
 
 // return a new array with unique items (duplicates removed)
