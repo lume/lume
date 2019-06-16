ef6974c24f8a36f6fef3fab5d2d23f0675aaebf5
Joe Pea <joe@trusktr.io>
2019-06-16 12:48:00 -0700

    convert Observable to a TypeScript class, with tests

--- src/core/Observable.ts
+++ src/core/Observable.ts
@@ -1,91 +1,76 @@
-import Class from 'lowclass'
-import Mixin from 'lowclass/Mixin'
+import {Mixin} from 'lowclass'
 
-const Brand = {}
-
-// TODO TS does it need to be `T extends Constructor<{}>` instead of `T extends typeof Object`?
+// XXX Could it need to be `T extends Constructor<{}>` instead of `T extends typeof Object`?
 // const ObservableMixin = <T extends typeof Object>(Base: T) => {
-const ObservableMixin = <T extends Constructor<{}>>(Base: T) => {
-    return Class('Observable').extends(
-        // (Base as unknown) as typeof Placeholder,
-        (Base as unknown) as typeof Object,
-        ({Private}) => ({
-            constructor() {},
+const ObservableMixin = <T extends Constructor>(Base: T) => {
+    return class Observable extends Base {
+        on(eventName: string, callback: Function, context: any) {
+            let eventMap = this.__eventMap
 
-            on(eventName: string, callback: Function, context: any) {
-                let __eventMap = this.__eventMap
+            if (!eventMap) eventMap = this.__eventMap = new Map()
 
-                if (!__eventMap) __eventMap = this.__eventMap = new Map()
+            let callbacks = eventMap.get(eventName)
 
-                let callbacks = __eventMap.get(eventName)
+            if (!callbacks) eventMap.set(eventName, (callbacks = []))
 
-                if (!callbacks) __eventMap.set(eventName, (callbacks = []))
+            if (typeof callback == 'function') callbacks.push([callback, context])
+            // save callback associated with context
+            else throw new Error('Expected a function in callback argument of Observable#on.')
+        }
 
-                if (typeof callback == 'function') callbacks.push([callback, context])
-                // save callback associated with context
-                else throw new Error('Expected a function in callback argument of Observable#on.')
-            },
+        off(eventName: string, callback: Function) {
+            const eventMap = this.__eventMap
 
-            off(eventName: string, callback: Function) {
-                const __eventMap = this.__eventMap
+            if (!eventMap) return
 
-                if (!__eventMap) return
+            const callbacks = eventMap.get(eventName)
 
-                const callbacks = __eventMap.get(eventName)
+            if (!callbacks) return
 
-                if (!callbacks) return
+            const index = callbacks.findIndex(tuple => tuple[0] === callback)
 
-                const index = callbacks.findIndex(tuple => tuple[0] === callback)
+            if (index == -1) return
 
-                if (index == -1) return
+            callbacks.splice(index, 1)
 
-                callbacks.splice(index, 1)
+            if (callbacks.length === 0) eventMap.delete(eventName)
 
-                if (callbacks.length === 0) __eventMap.delete(eventName)
+            if (eventMap.size === 0) this.__eventMap = null
+        }
 
-                if (__eventMap.size === 0) this.__eventMap = null
-            },
+        emit(eventName: string, data: any) {
+            const eventMap = this.__eventMap
 
-            emit(eventName: string, data: any) {
-                const __eventMap = this.__eventMap
+            if (!eventMap) return
 
-                if (!__eventMap) return
+            const callbacks = eventMap.get(eventName)
 
-                const callbacks = __eventMap.get(eventName)
+            if (!callbacks) return
 
-                if (!callbacks) return
+            let tuple: typeof callbacks[0]
+            let callback: typeof callbacks[0][0]
+            let context: typeof callbacks[0][1]
 
-                let tuple: typeof callbacks[0]
-                let callback: typeof callbacks[0][0]
-                let context: typeof callbacks[0][1]
+            for (let i = 0, len = callbacks.length; i < len; i += 1) {
+                tuple = callbacks[i]
+                callback = tuple[0]
+                context = tuple[1]
+                callback.call(context, data)
+            }
+        }
 
-                for (let i = 0, len = callbacks.length; i < len; i += 1) {
-                    tuple = callbacks[i]
-                    callback = tuple[0]
-                    context = tuple[1]
-                    callback.call(context, data)
-                }
-            },
+        // alias for emit
+        trigger(eventName: string, data: any) {
+            return this.emit(eventName, data)
+        }
 
-            // alias for emit
-            trigger(eventName: string, data: any) {
-                return this.emit(eventName, data)
-            },
+        // alias for emit
+        triggerEvent(eventName: string, data: any) {
+            return this.emit(eventName, data)
+        }
 
-            // alias for emit
-            triggerEvent(eventName: string, data: any) {
-                return this.emit(eventName, data)
-            },
-
-            protected: {Observable: 'Observable'},
-
-            private: {
-                __eventMap: null as null | Map<string, Array<[Function, any]>>,
-            },
-        }),
-        Brand
-    )
+        private __eventMap: Map<string, Array<[Function, any]>> | null = null
+    }
 }
 
-// TODO TS How to fix Mixin?
 export default Mixin(ObservableMixin)
