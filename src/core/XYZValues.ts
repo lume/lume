a3c5eebff3de2a4c93755ae9670c60ba1a389dc3
Joe Pea <joe@trusktr.io>
2019-07-15 23:55:50 -0700

    remove unused comments

--- src/core/XYZValues.ts
+++ src/core/XYZValues.ts
@@ -1,21 +1,11 @@
 import Observable from './Observable'
 import r from 'regexr'
 
-// StrictUnion<A | B | C>
-// see https://github.com/trusktr/typebox/blob/master/src/StrictUnion.ts
-// type UnionKeys<T> = T extends any ? keyof T : never
-// type StrictUnionHelper<T, TAll> = T extends any ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>> : never
-// type StrictUnion<T> = StrictUnionHelper<T, T>
-
 export type XYZValuesArray<T> = [T, T, T]
 export type XYZPartialValuesArray<T> = [T] | [T, T] | [T, T, T] // Is there a better way to make a tuplet from 1 to 3 items?
 export type XYZValuesObject<T> = {x: T; y: T; z: T}
 export type XYZPartialValuesObject<T> = Partial<XYZValuesObject<T>>
 export type XYZValuesParameters<T> = /*XYZValues | */ XYZPartialValuesArray<T> | XYZPartialValuesObject<T> | string | T
-// type XYZValuesParameters<T, TArray = XYZValuesArray<T>, TObject = XYZValuesObject<T>> = /*XYZValues | */ TArray | TObject | string | T
-// type XYZValuesParameters<T extends unknown> = StrictUnion<
-//     /*XYZValues | */ XYZValuesArray<T> | XYZValuesObject<T> | string | T
-// >
 
 const defaultValues: XYZValuesObject<any> = {x: undefined, y: undefined, z: undefined}
 
