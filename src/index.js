6e0712f17f10cb22f26472090ac3febefec8fce8
Joe Pea <joe@trusktr.io>
2019-07-28 17:57:11 -0700

    remove old famous stuff

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-import Calendar from './Calendar'
-import DoubleSidedPlane from './DoubleSidedPlane'
-import Grid from './Grid'
-import Molecule from './Molecule'
-import Plane from './Plane'
-import PushMenuLayout from './PushMenuLayout'
-
-import * as utils from './utils'
 import * as core from './core'
 import * as html from './html'
 import * as components from './components'
 
 export {
-    Calendar,
-    DoubleSidedPlane,
-    Grid,
-    Molecule,
-    Plane,
-    PushMenuLayout,
-
-    utils,
     core,
     html,
     components,
 }
 
-export const version = '21.0.6'
+export const version = '21.1.0'
