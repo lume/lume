8daa8b44c1e7f8763e785c9e4cf388ac839ea33f
Joe Pea <joe@trusktr.io>
2020-03-22 18:29:08 -0700

    remove unused files

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.mkShell {
-    buildInputs = with pkgs; [
-        hello
-        #(import ~/src/trusktr+nixpkgs/nodejs/default.nix {pkgs})."13.6.0"
-    ];
-  }
