0af511bf57a469bb39cf2c30e6273bea948cec20
Joe Pea <joe@trusktr.io>
2021-01-17 13:34:54 -0800

    import ShaderMaterialBehavior from Mesh, so it is available for meshes by default

--- packages/lume/src/core/Mesh.ts
+++ packages/lume/src/core/Mesh.ts
@@ -10,6 +10,7 @@ import type {Material} from 'three/src/materials/Material'
 // choose names for better flexibility. See TODO NAMING below.
 import '../html/behaviors/BasicMaterialBehavior'
 import '../html/behaviors/PhongMaterialBehavior'
+import '../html/behaviors/ShaderMaterialBehavior'
 import '../html/behaviors/DOMNodeMaterialBehavior'
 import '../html/behaviors/BoxGeometryBehavior'
 import '../html/behaviors/SphereGeometryBehavior'
