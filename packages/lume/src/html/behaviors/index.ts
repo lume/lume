1e7aac8187b4482e8d32e9f26361130717ad1973
Joe Pea <joe@trusktr.io>
2021-02-12 21:31:19 -0800

    export all behavior stuff so it is re-usable by consumers

--- packages/lume/src/html/behaviors/index.ts
+++ packages/lume/src/html/behaviors/index.ts
@@ -1,13 +1,25 @@
+// Abstract behavior base classes
+export * from './Behavior'
+export * from './RenderableBehavior'
+export * from './BaseMeshBehavior'
 export * from './BaseGeometryBehavior'
 export * from './BaseMaterialBehavior'
-export * from './BaseMeshBehavior'
+
+// Concrete behaviors
 export * from './BasicMaterialBehavior'
 export * from './BoxGeometryBehavior'
 export * from './DOMNodeGeometryBehavior'
 export * from './DOMNodeMaterialBehavior'
-export * from './DefaultBehaviors'
+export * from './GltfModelBehavior'
 export * from './ObjModelBehavior'
 export * from './PhongMaterialBehavior'
 export * from './PlaneGeometryBehavior'
+export * from './RoundedRectangleGeometryBehavior'
 export * from './ShaderMaterialBehavior'
 export * from './SphereGeometryBehavior'
+
+// Utility classes
+export * from './ForwardProps'
+
+// Internal detail
+// export * from './MaterialTexture'
