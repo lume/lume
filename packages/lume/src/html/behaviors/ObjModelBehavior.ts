8e7e6140ee142590c64678221099df35b775a8f5
Joe Pea <joe@trusktr.io>
2021-02-24 21:49:14 -0800

    improvements to OBJModelBehavior: better types, better cancellation of loading like other with other model behaviors

--- packages/lume/src/html/behaviors/ObjModelBehavior.ts
+++ packages/lume/src/html/behaviors/ObjModelBehavior.ts
@@ -1,5 +1,5 @@
 import 'element-behaviors'
-import {reactive, attribute, autorun, StopFunction} from '@lume/element'
+import {reactive, stringAttribute, autorun, StopFunction} from '@lume/element'
 import {disposeObjectTree, setRandomColorPhongMaterial, isRenderItem} from '../../utils/three.js'
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
 import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader.js'
@@ -8,6 +8,7 @@ import {RenderableBehavior} from './RenderableBehavior.js'
 
 import type {Object3D} from 'three/src/core/Object3D.js'
 import type BaseMaterialBehavior from './BaseMaterialBehavior.js'
+import type {Group} from 'three/src/objects/Group.js'
 
 // TODO move this somewhere better.
 declare global {
@@ -20,15 +21,19 @@ export type ObjModelBehaviorAttributes = 'obj' | 'mtl'
 
 @reactive
 export default class ObjModelBehavior extends RenderableBehavior {
-	@attribute obj = ''
-	@attribute mtl = ''
+	@stringAttribute('') obj = ''
+	@stringAttribute('') mtl = ''
 
-	// TODO no any
-	model: any = null
-	objLoader: any
-	mtlLoader: any
+	model?: Group
+	objLoader?: OBJLoader
+	mtlLoader?: MTLLoader
 
 	protected static _observedProperties = ['obj', 'mtl', ...(RenderableBehavior._observedProperties || [])]
+
+	// This is incremented any time we need a pending load() to cancel (f.e. on
+	// src change, or unloadGL cycle), so that the loader will ignore the
+	// result when a version change has happened.
+	private __version = 0
 
 	private __stopFns: StopFunction[] = []
 
@@ -52,6 +57,7 @@ export default class ObjModelBehavior extends RenderableBehavior {
 
 			if (!firstRun) this.__cleanupModel()
 
+			this.__version++
 			// TODO We can update only the material or model specifically
 			// instead of reloading the whole object.
 			this.__loadObj()
@@ -71,6 +77,9 @@ export default class ObjModelBehavior extends RenderableBehavior {
 
 		this.__cleanupModel()
 
+		// Increment this in case the loader is still loading, so it will ignore the result.
+		this.__version++
+
 		return true
 	}
 
@@ -85,67 +94,85 @@ export default class ObjModelBehavior extends RenderableBehavior {
 
 		this.__materialIsFromMaterialBehavior = false
 
-		this.model = null
+		this.model = undefined
 	}
 
 	private __loadObj() {
-		const {obj, mtl, mtlLoader, objLoader} = this
+		const {obj, mtl, mtlLoader, objLoader, __version} = this
 
 		if (!obj) return
 
 		if (mtl) {
-			mtlLoader.setResourcePath(mtl.substr(0, mtl.lastIndexOf('/') + 1))
+			mtlLoader!.setResourcePath(mtl.substr(0, mtl.lastIndexOf('/') + 1))
 
-			mtlLoader.load(mtl, (materials: any) => {
-				if (!this._glLoaded) return
+			mtlLoader!.load(mtl, materials => {
+				if (__version !== this.__version) return
 
 				materials.preload()
-				objLoader.setMaterials(materials)
-				objLoader.load(obj, (model: any) => {
-					if (!this._glLoaded) return
+
+				objLoader!.setMaterials(materials)
+				objLoader!.load(obj, model => {
+					if (__version !== this.__version) return
 
 					this.__setModel(model)
 				})
 			})
 		} else {
-			objLoader.load(obj, (model: any) => {
-				if (!this._glLoaded) return
+			objLoader!.load(
+				obj,
+				model => {
+					if (__version !== this.__version) return
 
-				// TODO Simplify this by getting based on type.
-				let materialBehavior = this.element.behaviors.get('basic-material') as BaseMaterialBehavior
-				if (!materialBehavior)
-					materialBehavior = this.element.behaviors.get('phong-material') as BaseMaterialBehavior
-				if (!materialBehavior)
-					materialBehavior = this.element.behaviors.get('standard-material') as BaseMaterialBehavior
-				if (!materialBehavior)
-					materialBehavior = this.element.behaviors.get('lambert-material') as BaseMaterialBehavior
+					// TODO Simplify this by getting based on type.
+					let materialBehavior = this.element.behaviors.get('basic-material') as BaseMaterialBehavior
+					if (!materialBehavior)
+						materialBehavior = this.element.behaviors.get('phong-material') as BaseMaterialBehavior
+					if (!materialBehavior)
+						materialBehavior = this.element.behaviors.get('standard-material') as BaseMaterialBehavior
+					if (!materialBehavior)
+						materialBehavior = this.element.behaviors.get('lambert-material') as BaseMaterialBehavior
 
-				if (materialBehavior) {
-					this.__materialIsFromMaterialBehavior = true
+					if (materialBehavior) {
+						this.__materialIsFromMaterialBehavior = true
 
-					// TODO this part only works on Mesh elements at the
-					// moment. We will update the geometry and material
-					// behaviors to work in tandem with or without a mesh
-					// behavior, and other behaviors can use the geometry or
-					// material features.
-					model.traverse((child: Object3D) => {
-						if (isRenderItem(child)) {
-							child.material = materialBehavior.getMeshComponent('material')
-						}
-					})
-				} else {
-					// if no material, make a default one with random color
-					setRandomColorPhongMaterial(model)
-				}
+						// TODO this part only works on Mesh elements at the
+						// moment. We will update the geometry and material
+						// behaviors to work in tandem with or without a mesh
+						// behavior, and other behaviors can use the geometry or
+						// material features.
+						model.traverse((child: Object3D) => {
+							if (isRenderItem(child)) {
+								child.material = materialBehavior.getMeshComponent('material')
+							}
+						})
+					} else {
+						// if no material, make a default one with random color
+						setRandomColorPhongMaterial(model)
+					}
 
-				this.__setModel(model)
-			})
+					this.__setModel(model)
+				},
+				progress => __version === this.__version && this.element.emit(Events.PROGRESS, progress),
+				error => __version === this.__version && this.__onError(error),
+			)
 		}
 	}
 
-	private __setModel(model: any) {
-		this.element.three.add((this.model = model))
-		this.element.emit(Events.MODEL_LOAD, {format: 'obj', model: model})
+	private __onError(error: ErrorEvent) {
+		const message =
+			error?.message ??
+			`Failed to load ${this.element.tagName.toLowerCase()} with obj value "${this.obj}" and mtl value "${
+				this.mtl
+			}".`
+
+		console.warn(message)
+		this.element.emit(Events.MODEL_ERROR, error.error)
+	}
+
+	private __setModel(model: Group) {
+		this.model = model
+		this.element.three.add(model)
+		this.element.emit(Events.MODEL_LOAD, {format: 'obj', model})
 		this.element.needsUpdate()
 	}
 }
