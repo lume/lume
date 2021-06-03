ecd048591acad5285a97d783f14ada145627dd92
Joe Pea <joe@trusktr.io>
2021-06-03 11:34:46 -0700

    attempted to convert to JSX in LoadingIcon, but it is not ready yet. I left TODO comments

--- packages/lume/src/examples/LoadingIcon.ts
+++ packages/lume/src/examples/LoadingIcon.ts
@@ -1,3 +1,5 @@
+/* TODO @jsxImportSource @lume/element */
+
 // Find a standalone version of this loading icon at
 // https://codepen.io/trusktr/pen/poNxzqJ
 
@@ -16,19 +18,29 @@ export class LoadingIcon extends Element {
 
 	template = () => html`
 		<>
-			<div top></div>
-			<div right></div>
-			<div bottom></div>
-			<div left></div>
+			<div class="top"></div>
+			<div class="right"></div>
+			<div class="bottom"></div>
+			<div class="left"></div>
 		</>
 	`
+
+	// TODO convert to JSX.
+	// template = () => (
+	// 	<>
+	// 		<div class="top"></div>
+	// 		<div class="right"></div>
+	// 		<div class="bottom"></div>
+	// 		<div class="left"></div>
+	// 	</>
+	// )
 
 	css = /*css*/ `
 		:host {
 			--loading-icon-color: ${this.color};
 		}
 
-		[top], [right], [bottom], [left] {
+		.top, .right, .bottom, .left {
 			position: absolute;
 			width: 100%;
 			height: 100%;
@@ -36,19 +48,19 @@ export class LoadingIcon extends Element {
 			opacity: 0;
 			border: 8px solid rgba(var(--loading-icon-color), 0);
 		}
-		[top] {
+		.top {
 			border-top-color: rgba(var(--loading-icon-color), 1);
 			animation: fade 1s 0s infinite;
 		}
-		[right] {
+		.right {
 			border-right-color: rgba(var(--loading-icon-color), 1);
 			animation: fade 1s 0.25s infinite;
 		}
-		[bottom] {
+		.bottom {
 			border-bottom-color: rgba(var(--loading-icon-color), 1);
 			animation: fade 1s 0.5s infinite;
 		}
-		[left] {
+		.left {
 			border-left-color: rgba(var(--loading-icon-color), 1);
 			animation: fade 1s 0.75s infinite;
 		}
