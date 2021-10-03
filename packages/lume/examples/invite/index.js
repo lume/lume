998499a3fdf2e907c50d7083a549c97f5ea47886
Joe Pea <joe@trusktr.io>
2021-10-03 02:19:22 -0700

    switch the foliage back to CSS, Three.js has some sort of random glitch bug when rendering the transparent texture

--- packages/lume/examples/invite/index.js
+++ packages/lume/examples/invite/index.js
@@ -237,7 +237,7 @@ async function makeParticles() {
 const {element} = LUME
 
 element('lume-svg')(
-	class Svg extends LUME.Element {
+	class Svg extends LUME.Node {
 		static get observedAttributes() {
 			return {
 				src: LUME.attribute.string,
@@ -246,6 +246,70 @@ element('lume-svg')(
 
 		_loadGL() {
 			if (!super._loadGL()) return false
+
+			const loader = new THREE.SVGLoader()
+
+			loader.load(this.src, data => {
+				const paths = data.paths
+
+				const group = new THREE.Group()
+				// group.scale.multiplyScalar(0.25)
+				// group.position.x = -70
+				// group.position.y = 70
+				// group.scale.y *= -1
+
+				for (let i = 0; i < paths.length; i++) {
+					const path = paths[i]
+
+					const fillColor = path.userData.style.fill
+					if (fillColor !== undefined && fillColor !== 'none') {
+						const material = new THREE.MeshBasicMaterial({
+							color: new THREE.Color().setStyle(fillColor),
+							opacity: path.userData.style.fillOpacity,
+							transparent: path.userData.style.fillOpacity < 1,
+							side: THREE.DoubleSide,
+							depthWrite: false,
+						})
+
+						const shapes = SVGLoader.createShapes(path)
+
+						for (let j = 0; j < shapes.length; j++) {
+							const shape = shapes[j]
+
+							const geometry = new THREE.ShapeGeometry(shape)
+							const mesh = new THREE.Mesh(geometry, material)
+
+							group.add(mesh)
+						}
+					}
+
+					const strokeColor = path.userData.style.stroke
+
+					if (strokeColor !== undefined && strokeColor !== 'none') {
+						const material = new THREE.MeshBasicMaterial({
+							color: new THREE.Color().setStyle(strokeColor),
+							opacity: path.userData.style.strokeOpacity,
+							transparent: path.userData.style.strokeOpacity < 1,
+							side: THREE.DoubleSide,
+							depthWrite: false,
+						})
+
+						for (let j = 0, jl = path.subPaths.length; j < jl; j++) {
+							const subPath = path.subPaths[j]
+
+							const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path.userData.style)
+
+							if (geometry) {
+								const mesh = new THREE.Mesh(geometry, material)
+
+								group.add(mesh)
+							}
+						}
+					}
+				}
+
+				this.three.add(group)
+			})
 
 			return true
 		}
