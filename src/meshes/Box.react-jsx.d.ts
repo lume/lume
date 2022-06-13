d699a3c3a0806b62e624c58dbbe632d4078b9837
Joe Pea <joe@trusktr.io>
2022-06-12 23:05:48 -0700

    fix: type import path for React types was incorrect

--- src/meshes/Box.react-jsx.d.ts
+++ src/meshes/Box.react-jsx.d.ts
@@ -1,6 +1,9 @@
 import type {Box, BoxAttributes} from './Box'
 import type {ReactElementAttributes} from '@lume/element/src/react'
-import type {PhongMaterialBehavior, PhongMaterialBehaviorAttributes} from '../behaviors/materials/PhongMaterialBehavior'
+import type {
+	PhongMaterialBehavior,
+	PhongMaterialBehaviorAttributes,
+} from '../behaviors/mesh-behaviors/materials/PhongMaterialBehavior'
 
 // React users can import this to have appropriate types for the element in their JSX markup.
 declare global {
