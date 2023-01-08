cfa47467bfdbc33736411503911e021650c73b79
Joe Pea <joe@trusktr.io>
2023-01-07 16:40:22 -0800

    chore: follow convention that an element's template followed by its style goes at the bottom of a custom element class

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -587,76 +587,6 @@ export class Scene extends SharedAPI {
 		this.needsUpdate()
 	}
 
-	static override css = /*css*/ `
-		${
-			// FIXME super.css is not handled properly by our Babel setup, fix it or remove that feature.
-			// super.css
-			SharedAPI.css
-		}
-
-		:host {
-			/*
-			 * A Scene is strict: it does not leak content, its rendering is not
-			 * affected by external layout, and its size is not affected by its
-			 * content. It is an absolutely contained drawing area.
-			 */
-			contain: size layout paint; /*fallback, TODO remove once Safari is caught up*/
-			contain: strict; /*override*/
-			overflow: hidden;
-			position: static; /*override*/
-		}
-
-		/* The purpose of this is to contain the position:absolute layers so they don't break out of the Scene layout. */
-		.container {
-			position: relative
-		}
-
-		.container,
-		.CSS3DLayer,
-		.MiscellaneousLayer,
-		.WebGLLayer,
-		.WebGLLayer > canvas  {
-			margin: 0; padding: 0;
-			width: 100%; height: 100%;
-			display: block;
-		}
-
-		.CSS3DLayer,
-		.MiscellaneousLayer,
-		.WebGLLayer {
-			/* make sure all layers are stacked on top of each other */
-			position: absolute; top: 0; left: 0;
-		}
-
-		.CSS3DLayer {
-			transform-style: preserve-3d;
-		}
-
-		.container {
-			pointer-events: none;
-		}
-
-		.MiscellaneousLayer > * {
-			/* Allow children of the Misc layer to have pointer events.	Needed for the WebXR button, for example */
-			pointer-events: auto;
-		}
-
-		/*
-		 * This trick is needed in Firefox to remove pointer events from the
-		 * transparent cameraElement from interfering with pointer events on the
-		 * scene objects. We do not wish to interact with this element anyway, as
-		 * it serves only for positioning the view.
-		 */
-		.cameraElement > * {
-			pointer-events: auto;
-		}
-
-		.vrButton {
-			color: black;
-			border-color: black;
-		}
-	`
-
 	// WebGLRendererThree appends its content into here.
 	_glLayer: HTMLDivElement | null = null
 
@@ -665,32 +595,6 @@ export class Scene extends SharedAPI {
 
 	// Miscellaneous layer. The "Enter VR/AR" button is placed here by Scene, for example.
 	_miscLayer: HTMLDivElement | null = null
-
-	override template = () => html`
-		<div class="container">
-			<div
-				ref=${(el: any) => (this._cssLayer = el)}
-				class="CSS3DLayer"
-				style=${() => (this.swapLayers ? 'z-index: 1' : '')}
-			>
-				${
-					/* WebGLRendererThree places the CSS3DRendererNested domElement
-					here, which contains a <slot> element that child elements of
-					a Scene are distributed into (rendered relative to).
-					*/ ''
-				}
-			</div>
-
-			<div ref=${(el: any) => (this._glLayer = el)} class="WebGLLayer">
-				${/* WebGLRendererThree places the Three.js <canvas> element here. */ ''}
-			</div>
-
-			<div ref=${(el: any) => (this._miscLayer = el)} class="MiscellaneousLayer">
-				${/* This layer is used by WebXR to insert UI like the Enter VR/AR button. */ ''}
-				<slot name="misc"></slot>
-			</div>
-		</div>
-	`
 
 	drawScene() {
 		this.#glRenderer && this.#glRenderer.drawScene(this)
@@ -1254,6 +1158,102 @@ export class Scene extends SharedAPI {
 			this.__elementParentSize = parentSize
 		}
 	}
+
+	override template = () => html`
+		<div class="container">
+			<div
+				ref=${(el: any) => (this._cssLayer = el)}
+				class="CSS3DLayer"
+				style=${() => (this.swapLayers ? 'z-index: 1' : '')}
+			>
+				${
+					/* WebGLRendererThree places the CSS3DRendererNested domElement
+					here, which contains a <slot> element that child elements of
+					a Scene are distributed into (rendered relative to).
+					*/ ''
+				}
+			</div>
+
+			<div ref=${(el: any) => (this._glLayer = el)} class="WebGLLayer">
+				${/* WebGLRendererThree places the Three.js <canvas> element here. */ ''}
+			</div>
+
+			<div ref=${(el: any) => (this._miscLayer = el)} class="MiscellaneousLayer">
+				${/* This layer is used by WebXR to insert UI like the Enter VR/AR button. */ ''}
+				<slot name="misc"></slot>
+			</div>
+		</div>
+	`
+
+	static override css = /*css*/ `
+		${
+			// FIXME super.css is not handled properly by our Babel setup, fix it or remove that feature.
+			// super.css
+			SharedAPI.css
+		}
+
+		:host {
+			/*
+			 * A Scene is strict: it does not leak content, its rendering is not
+			 * affected by external layout, and its size is not affected by its
+			 * content. It is an absolutely contained drawing area.
+			 */
+			contain: size layout paint; /*fallback, TODO remove once Safari is caught up*/
+			contain: strict; /*override*/
+			overflow: hidden;
+			position: static; /*override*/
+		}
+
+		/* The purpose of this is to contain the position:absolute layers so they don't break out of the Scene layout. */
+		.container {
+			position: relative
+		}
+
+		.container,
+		.CSS3DLayer,
+		.MiscellaneousLayer,
+		.WebGLLayer,
+		.WebGLLayer > canvas  {
+			margin: 0; padding: 0;
+			width: 100%; height: 100%;
+			display: block;
+		}
+
+		.CSS3DLayer,
+		.MiscellaneousLayer,
+		.WebGLLayer {
+			/* make sure all layers are stacked on top of each other */
+			position: absolute; top: 0; left: 0;
+		}
+
+		.CSS3DLayer {
+			transform-style: preserve-3d;
+		}
+
+		.container {
+			pointer-events: none;
+		}
+
+		.MiscellaneousLayer > * {
+			/* Allow children of the Misc layer to have pointer events.	Needed for the WebXR button, for example */
+			pointer-events: auto;
+		}
+
+		/*
+		 * This trick is needed in Firefox to remove pointer events from the
+		 * transparent cameraElement from interfering with pointer events on the
+		 * scene objects. We do not wish to interact with this element anyway, as
+		 * it serves only for positioning the view.
+		 */
+		.cameraElement > * {
+			pointer-events: auto;
+		}
+
+		.vrButton {
+			color: black;
+			border-color: black;
+		}
+	`
 }
 
 // Put initial value on the prototype to make it available during construction
