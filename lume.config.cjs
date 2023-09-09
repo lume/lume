250b21d6d5b5574b121b2abad741b8686390dee2
Joe Pea <joe@trusktr.io>
2023-09-09 01:32:50 -0700

    update lume cli to remove ancient electron and karma-electron to avoid having to build electron in every OS with potential failure and to avoid having to use older JS syntax

--- lume.config.cjs
+++ lume.config.cjs
@@ -1,3 +1,9 @@
 module.exports = {
 	globalName: 'LUME',
+
+	/**
+	 * Override this for the CLI, so that VS Code uses the default one, and CLI
+	 * applies only to the top-level repo and not any of the sub-packages.
+	 */
+	prettierIgnorePath: '.prettierignore.lumecli',
 }
