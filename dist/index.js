8bdd6d19809ae2f933edd47fea15d0593fd57708
Joe Pea <joe@trusktr.io>
2023-10-31 17:46:21 -0700

    v0.3.0-alpha.29

--- dist/index.js
+++ dist/index.js
@@ -19,5 +19,5 @@ export * from './textures/index.js';
 export * from './utils/index.js';
 export * from './xyz-values/index.js';
 export * from './defineElements.js';
-export const version = '0.3.0-alpha.28';
+export const version = '0.3.0-alpha.29';
 //# sourceMappingURL=index.js.map
